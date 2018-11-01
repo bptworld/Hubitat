c907ef35a52b8e0e0f007df577ed90ddb16e6c03
bptworld <bryan@bptworld.com>
2018-11-01 16:07:20 -0400

    Update SIP2IR Child.groovy

--- Send IP2IR/SIP2IR Child.groovy
+++ Send IP2IR/SIP2IR Child.groovy
@@ -39,11 +39,13 @@
  *
  *  Changes:
  *
- *  
+ *  V1.1.2 - 11/01/18 - Added an optional Digit 4 within Channels. Sending Enter Code after Digits is now optional. Made the
+ *                      Delay between sending digits user specified and added in some instructions.
+ *  V1.1.1 - 10/29/18 - Updated Channels to be either a Button or a Switch, only Switches can be used with Google Assistant.
  *  V1.1.0 - 10/20/18 - Big change in how Channels work. Only have to enter each digits IR code once, in the Advance Section of
- *			the Parent app. Now in the Child apps, only need to put in the digits (no IR codes!). This is a 
- *			non-destructive update. All existing channels will still work. Thanks to Bruce (@bravenel) for showing
- *			me how to send code from parent to child apps.
+ *			 			the Parent app. Now in the Child apps, only need to put in the digits (no IR codes!). This is a 
+ *			 			non-destructive update. All existing channels will still work. Thanks to Bruce (@bravenel) for showing
+ *						me how to send code from parent to child apps.
  *  V1.0.0 - 10/15/18 - Initial release
  */
  
@@ -66,35 +68,64 @@ preferences {
 		section() {
      	   paragraph "This app is designed to send commands to an iTach IP2IR device."
  	    }
+		section("Instructions:", hideable: true, hidden: true) {
+				paragraph "There are 4 types of Triggers that can be made."
+        		paragraph "<b>Switch:</b><br>To turn anything on/off. ie. Television, Stereo, Cable Box, etc. Remember, it's okay to put the same code in box on and off if necessary."
+    			
+        		paragraph "<b>Button:</b><br>Used to send just one command. ie. Volume Up, Channel Down, etc. Note: this can not be used with Google Assistant."
+        		
+        		paragraph "<b>Channel_Switch:</b><br>Used to send 1 to 4 commands at the same time.  ie. Channels, Volume(x2, x3, x4), etc."
+        		
+            	paragraph "<b>Channel_Button:</b><br>Also, used to send 1 to 4 commands at the same time.  ie. Channels, Volume(x2, x3, x4), etc. Note: this can not be used with Google Assistant."
+        		
+				paragraph "<b>Important:</b><br>Each child app takes a device to trigger the commands, so be sure to create either a Virtual Switch or Virtual Button before trying to create a child app."
+				
+				paragraph "<b>Google Assistant Notes:</b><br>Google Assistant only works with switches. If creating virtual switches for channels, be sure to use the 'Enable auto off' @ '500ms' to give the effect of a button in a Dashboard but still be able to tell Google to control it."
+		}
 		section() {
-            input "triggerMode", "enum", required: true, title: "Select Trigger Type", submitOnChange: true,  options: ["Button", "Switch", "Channel"] 
+			paragraph "<b>Select the Trigger Type to Activate to send the IR command</b>"
+            input "triggerMode", "enum", required: true, title: "Select Trigger Type", submitOnChange: true,  options: ["Button", "Switch", "Channel_Switch", "Channel_Button"]
+			
             if(triggerMode == "Switch"){
-                section("Select a 'Virtual Switch Device' to Activate the Command") {
+                section("<b>Select a 'Virtual Switch Device' to Activate the Command</b>") {
                 	input "switch1", "capability.switch", title: "Select Trigger Device", required: true, multiple: false
             		input "msgToSendOn", "text", required: true, title: "IR Code to Send - ON", defaultValue: "sendir..."
                 	input "msgToSendOff", "text", required: true, title: "IR Code to Send - OFF", defaultValue: "sendir..."
                 }
             }
             if(triggerMode == "Button"){
-                section("Select a 'Virtual Button Device' to Activate the Command") {
+                section("<b>Select a 'Virtual Button Device' to Activate the Command</b>") {
                 	input "button1", "capability.pushableButton", title: "Select Button Device", required: true, multiple: false
             		input "buttonNumber", "enum", title: "Enter Button Number", required: true, options: ["1", "2", "3", "4", "5"]
                 	input "msgToSendPushed", "text", required: true, title: "IR Code to Send on Push", defaultValue: "sendir..."
                 }
             }
-            if(triggerMode == "Channel"){
-                
-                section("Select a 'Virtual Button Device' to Activate the Channel") {
-					input "button1", "capability.pushableButton", title: "Select Button Device", required: true, multiple: false
-            		input "buttonNumber", "enum", title: "Enter Button Number", required: true, options: ["1", "2", "3", "4", "5"]
-					
+            if(triggerMode == "Channel_Switch"){
+				section("<b>Select a 'Virtual Switch Device' to Activate the Command</b>") {
+					input "switch2", "capability.switch", title: "Select Trigger Device", required: true, multiple: false
+					paragraph "<b>Input between 1 and 4 digits to send.</b>"
             		input "Digit1", "text", required: true, title: "Channel - First Digit", defaultValue: ""
-            		input "Digit2", "text", required: true, title: "Channel - Second Digit", defaultValue: ""
-            		input "Digit3", "text", required: true, title: "Channel - Third Digit", defaultValue: ""
-        		}
+            		input "Digit2", "text", required: false, title: "Channel - Second Digit", defaultValue: ""
+            		input "Digit3", "text", required: false, title: "Channel - Third Digit", defaultValue: ""
+					input "Digit4", "text", required: fasle, title: "Channel - Fourth Digit", defaultValue: ""
+					input "Delay", "number", required: true, title: "Delay between sending Digits", defaultValue: 1000
+					input "EnterCode", "bool", title: "Send Enter Code after Digits", required: true, defaultValue: false
+				}
+			}
+			if(triggerMode == "Channel_Button"){
+				section("<b>Select a 'Virtual Button Device' to Activate the Command</b>") {
+					input "button1", "capability.pushableButton", title: "Select Button Device", required: false, multiple: false
+            		input "buttonNumber", "enum", title: "Enter Button Number", required: true, options: ["1", "2", "3", "4", "5"]
+					paragraph "<b>Input between 1 and 4 digits to send.</b>"
+            		input "Digit1", "text", required: true, title: "Channel - First Digit", defaultValue: ""
+            		input "Digit2", "text", required: false, title: "Channel - Second Digit", defaultValue: ""
+            		input "Digit3", "text", required: false, title: "Channel - Third Digit", defaultValue: ""
+					input "Digit4", "text", required: fasle, title: "Channel - Fourth Digit", defaultValue: ""
+					input "Delay", "number", required: true, title: "Delay between sending Digits", defaultValue: 1000
+					input "EnterCode", "bool", title: "Send Enter Code after Digits", required: true, defaultValue: false
+				}
             }
-    }
-	
+		}
     	section() {
             input "speaker", "capability.speechSynthesis", title: "Select the 'IP2IR Telnet' device created during initial setup/install.", required: true, multiple: false
     		input "ipaddress", "text", required: true, title: "iTach IP2IR IP Adress", defaultValue: "0.0.0.0"
@@ -116,6 +147,8 @@ def updated() {
 
 def initialize(){
     
+	LOGDEBUG("Inside Initialize...")
+			 
     myMsgDigit1()
     myMsgDigit2()
     myMsgDigit3()
@@ -128,9 +161,13 @@ def initialize(){
     myMsgDigit0()
     myMsgDigitE()
     
-    if(triggerMode == "Switch"){subscribe(switch1, "switch", switchHandler1)}
+    if(triggerMode == "Switch"){
+		LOGDEBUG("Initialize... triggerMode=Switch")
+		subscribe(switch1, "switch", switchHandler1)
+	}
  
     if(triggerMode == "Button"){
+		LOGDEBUG("Initialize... triggerMode=Button")
         if(buttonNumber == '1'){subscribe(button1, "pushed.1", buttonHandler1)}
         if(buttonNumber == '2'){subscribe(button1, "pushed.2", buttonHandler2)}
         if(buttonNumber == '3'){subscribe(button1, "pushed.3", buttonHandler3)}
@@ -138,13 +175,18 @@ def initialize(){
         if(buttonNumber == '5'){subscribe(button1, "pushed.5", buttonHandler5)}
     }
     
-    if(triggerMode == "Channel"){
+    if(triggerMode == "Channel_Button"){
+		LOGDEBUG("Initialize... triggerMode=Channel_Button")
         if(buttonNumber == '1'){subscribe(button1, "pushed.1", channelHandler1)}
         if(buttonNumber == '2'){subscribe(button1, "pushed.2", channelHandler1)}
         if(buttonNumber == '3'){subscribe(button1, "pushed.3", channelHandler1)}
         if(buttonNumber == '4'){subscribe(button1, "pushed.4", channelHandler1)}
         if(buttonNumber == '5'){subscribe(button1, "pushed.5", channelHandler1)}
-    }
+	}
+	if(triggerMode == "Channel_Switch"){
+		LOGDEBUG("Initialize... triggerMode=Channel_Switch")
+		subscribe(switch2, "switch", channelHandlerSwitch)
+	}
 }
 
 def switchHandler1 (evt) {
@@ -346,10 +388,101 @@ def PresetToSend3(){
     LOGDEBUG("Getting Digit 3...${Digit3} - ${msgToSend3}")
 }
 
+def PresetToSend4(){
+    if(Digit4 == "1") {
+       myMsgDigit1()
+       msgToSend4 = parent.msgDigit1
+    }
+    if(Digit4 == "2") {
+       myMsgDigit2()
+       msgToSend4 = parent.msgDigit2
+    }
+    if(Digit4 == "3") {
+       myMsgDigit3()
+       msgToSend4 = parent.msgDigit3
+    }
+    if(Digit4 == "4") {
+       myMsgDigit4()
+       msgToSend4 = parent.msgDigit4
+    }
+    if(Digit4 == "5") {
+       myMsgDigit5()
+       msgToSend4 = parent.msgDigit5
+    }
+	if(Digit4 == "6") {
+       myMsgDigit6()
+       msgToSend4 = parent.msgDigit6
+    }
+	if(Digit4 == "7") {
+       myMsgDigit7()
+       msgToSend4 = parent.msgDigit7
+    }
+	if(Digit4 == "8") {
+       myMsgDigit8()
+       msgToSend4 = parent.msgDigit8
+    }
+	if(Digit4 == "9") {
+       myMsgDigit9()
+       msgToSend4 = parent.msgDigit9
+    }
+	if(Digit4 == "0") {
+       myMsgDigit0()
+       msgToSend4 = parent.msgDigit0
+    }
+    LOGDEBUG("Getting Digit 3...${Digit3} - ${msgToSend3}")
+}
+
 def PresetToSendS(){
     myMsgDigitE()
     msgToSendE = parent.msgDigitE
     LOGDEBUG("Getting Digit E...${PresetToSendS} - ${msgToSendE}")
+}
+
+def channelHandlerSwitch(evt) {
+def switching = evt.value
+    if(switching == "on"){
+        LOGDEBUG("You pressed Channel Switch On")
+    	PresetToSend1()
+    	PresetToSend2()
+		PresetToSend3()
+		PresetToSend4()
+   		PresetToSendS()
+    
+		LOGDEBUG("Inside channelHandlerSwitch Digits ${Digit1} ${Digit2} ${Digit3} ${Digit4} ${EnterCode}")
+	
+		LOGDEBUG("Msg to send Digit One: ${Digit1} - ${msgToSend1}")
+		speaker.speak(msgToSend1)
+    	pauseExecution(Delay)
+
+		if(Digit2 != "null") {
+    		LOGDEBUG("Msg to send Digit Two: ${Digit2} - ${msgToSend2}")
+			speaker.speak(msgToSend2)
+			pauseExecution(Delay)
+		} else{
+			LOGDEBUG("Did not send Channel Digit 2")
+		}
+		if(Digit3 != "null") {
+    		LOGDEBUG("Msg to send Digit Three: ${Digit3} - ${msgToSend3}")
+			speaker.speak(msgToSend3)
+    		pauseExecution(Delay)
+		} else{
+			LOGDEBUG("Did not send Channel Digit 3")
+		}
+		if(Digit4 != "null") {
+    		LOGDEBUG("Msg to send Digit Four: ${Digit4} - ${msgToSend4}")
+			speaker.speak(msgToSend4)
+    		pauseExecution(Delay)
+		} else{
+			LOGDEBUG("Did not send Channel Digit 4")
+		}
+		LOGDEBUG("${EnterCode}")			 
+		if(EnterCode == true) {
+    		LOGDEBUG("Msg to send Enter: ${EnterCode} - ${msgToSendE}")
+    		speaker.speak(msgToSendE)
+		} else{
+			LOGDEBUG("Did not send Channel Enter")
+		}
+	}
 }
 
 def channelHandler1(evt) {
@@ -357,19 +490,43 @@ def channelHandler1(evt) {
     	PresetToSend1()
     	PresetToSend2()
     	PresetToSend3()
+		PresetToSend4()
     	PresetToSendS()
-    	
-        LOGDEBUG("Msg to send Digit One: ${Digit1} - ${msgToSend1}")
-		speaker.speak(msgToSend1)
-    	pauseExecution(500)
+    
+	LOGDEBUG("Inside channelHandler1 Digits ${Digit1} ${Digit2} ${Digit3} ${Digit4} ${EnterCode}")
+	
+	LOGDEBUG("Msg to send Digit One: ${Digit1} - ${msgToSend1}")
+	speaker.speak(msgToSend1)
+    pauseExecution(1000)
+
+	if(Digit2 != "null") {
     	LOGDEBUG("Msg to send Digit Two: ${Digit2} - ${msgToSend2}")
 		speaker.speak(msgToSend2)
-    	pauseExecution(500)
+		pauseExecution(1000)
+	} else{
+		LOGDEBUG("Did not send Channel Digit 2")
+	}
+	if(Digit3 != "null") {
     	LOGDEBUG("Msg to send Digit Three: ${Digit3} - ${msgToSend3}")
 		speaker.speak(msgToSend3)
-    	pauseExecution(500)
+    	pauseExecution(1000)
+	} else{
+		LOGDEBUG("Did not send Channel Digit 3")
+	}
+	if(Digit4 != "null") {
+    	LOGDEBUG("Msg to send Digit Four: ${Digit4} - ${msgToSend4}")
+		speaker.speak(msgToSend4)
+    	pauseExecution(1000)
+	} else{
+		LOGDEBUG("Did not send Channel Digit 4")
+	}
+				 
+	if(Delay == "true") {
     	LOGDEBUG("Msg to send Enter: ${DigitE} - ${msgToSendE}")
     	speaker.speak(msgToSendE)
+	} else{
+		LOGDEBUG("Did not send Channel Enter")
+	}
 }
 
 def channelHandler2(evt) {
@@ -474,7 +631,7 @@ def LOGDEBUG(txt){
 }
 
 def display(){
-	section{paragraph "Version: 1.1.0<br>@BPTWorld"}     
+	section{paragraph "Version: 1.1.2<br>@BPTWorld"}     
 }
 
 def setVersion(){
