53cd8dbaf8bd4c1ecfdee12e6ee82772d1ecae0c
bptworld <bryan@bptworld.com>
2018-12-10 22:30:40 -0500

    Create OaaT-Child.groovy

---
+++ Apps/One at a Time/OaaT-Child.groovy
@@ -0,0 +1,159 @@
+/**
+ *  ****************  One at a Time Child ****************
+ *
+ *  Design Usage:
+ *  This app is designed to allow only one switch, in a group of switches, to be on at a time.
+ *	When one switch is turned on, the other swithes in the group will turn off.
+ *
+ *  Copyright 2018 Bryan Turcotte (@bptworld)
+ *
+ *  Special thanks to (@Cobra) for use of his Parent/Child code and various other bits and pieces.
+ *  Also thanks to Jody Albritton for the original 'One At A Time Please' code that I based this app off of.
+ *  
+ *  This App is free.  If you like and use this app, please be sure to give a shout out on the Hubitat forums to let
+ *  people know that it exists!  Thanks.
+ *
+ *  Remember...I am not a programmer, everything I do takes a lot of time and research!
+ *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
+ *
+ *  Paypal at: https://paypal.me/bptworld
+ *
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  If modifying this project, please keep the above header intact and add your comments/credits below - Thank you! -  @BPTWorld
+ *
+ *  App and Driver updates can be found at https://github.com/bptworld/Hubitat
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  Changes:
+ *
+ *  V1.1.0 - 12/10/18 - Changed over to Parent/Child type app. Create as many groups as you like. Also added in all the normal
+ *						stuff: Enable/Disable switch, Pause switch and Logging options.
+ *  V1.0.0 - 12/09/18 - Hubitat Port of ST app 'One at a Time Please' - 2015 Jody Albritton
+ *
+ */
+
+definition(
+    name: "One at a Time Child",
+    namespace: "BPTWorld",
+    author: "Bryan Turcotte",
+    description: "This app is designed to allow only one switch, in a group of switches, to be on at a time.",
+    category: "",
+    
+parent: "BPTWorld:One at a Time",
+    
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "",
+    )
+
+preferences {
+    page(name: "pageConfig")
+}
+
+def pageConfig() {
+    dynamicPage(name: "", title: "", install: true, uninstall: true, refreshInterval:0) {	
+    display()
+		section("Instructions:", hideable: true, hidden: true) {
+			paragraph "<b>Notes:</b>"
+			paragraph "* When one switch is turned on, the other switches in the group will turn off.<br>* Create as many groups as you like.<br>* Great for making sure only one scene is active at a time!"	
+		}
+   		section("Define Switch Group") {
+			input "switches", "capability.switch", title: "Select the switches to group", required: true, multiple: true
+		} 
+		section() {label title: "Enter a name for this automation", required: false}
+		section() {
+			input(name: "enablerSwitch1", type: "capability.switch", title: "Enable/Disable child app with this switch - If Switch is ON then app is disabled, if Switch is OFF then app is active.", required: false, multiple: false)
+		}
+        section() {
+            input(name: "debugMode", type: "bool", defaultValue: "true", title: "Enable Debug Logging", description: "Enable extra logging for debugging.")
+		}
+	}
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+	switches.each { device ->
+    	device.off()
+    }
+	initialize()
+}
+
+def updated() {	
+    LOGDEBUG("Updated with settings: ${settings}")
+    unsubscribe()
+	logCheck()
+	initialize()
+}
+
+def initialize() {
+	subscribe(switches, "switch.on", eventHandler)
+}
+
+def eventHandler(evt) {
+    switches.each { device ->
+        def target = device.toString()
+        def current = evt.device.toString()
+        if (target != current) {
+			LOGDEBUG("Turning ${target} off because ${current} is on")
+        	device.off()
+        }
+    }	
+}
+
+def pauseOrNot(){
+	LOGDEBUG("In pauseOrNot...")
+    state.pauseNow = pause1
+        if(state.pauseNow == true){
+            state.pauseApp = true
+            if(app.label){
+            if(app.label.contains('red')){
+                log.warn "Paused"}
+            else{app.updateLabel(app.label + ("<font color = 'red'> (Paused) </font>" ))
+              LOGDEBUG("App Paused - state.pauseApp = $state.pauseApp ")   
+            }
+            }
+        }
+     if(state.pauseNow == false){
+         state.pauseApp = false
+         if(app.label){
+     if(app.label.contains('red')){ app.updateLabel(app.label.minus("<font color = 'red'> (Paused) </font>" ))
+     	LOGDEBUG("App Released - state.pauseApp = $state.pauseApp ")                          
+        }
+     }
+  }    
+}
+
+def logCheck(){
+	state.checkLog = debugMode
+	if(state.checkLog == true){
+		log.info "${app.label} - All Logging Enabled"
+	}
+	else if(state.checkLog == false){
+		log.info "${app.label} - Further Logging Disabled"
+	}
+}
+
+def LOGDEBUG(txt){
+    try {
+		if (settings.debugMode) { log.debug("${app.label} - ${txt}") }
+    } catch(ex) {
+    	log.error("${app.label} - LOGDEBUG unable to output requested data!")
+    }
+}
+
+def display(){
+	section{paragraph "<b>One at a Time</b><br>App Version: 1.1.0<br>@BPTWorld"}      
+	section(){input "pause1", "bool", title: "Pause This App", required: true, submitOnChange: true, defaultValue: false }
+}
