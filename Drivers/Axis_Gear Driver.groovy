7f09fe0262e987a5465d7ab14b5a194c49373b21
bptworld <bryan@bptworld.com>
2019-01-05 09:44:24 -0500

    Update Axis_Gear Driver.groovy

--- Drivers/Axis_Gear Driver.groovy
+++ Drivers/Axis_Gear Driver.groovy
@@ -1,5 +1,5 @@
 metadata {
-    definition (name: "AXIS Gear - Hubitat", namespace: "axis", author: "AXIS Labs") {  
+    definition (name: "AXIS Gear-V3 Hubitat", namespace: "axis", author: "AXIS Labs") {  
         capability "Actuator"
         capability "Configuration"
         capability "Switch"
@@ -12,16 +12,17 @@ metadata {
         //Custom Commandes to achieve 25% increment control
         command "ShadesUp"
         command "ShadesDown"
-        
-                
+           
         fingerprint profileId: "0200", inClusters: "0000, 0001, 0004, 0005, 0006, 0008, 0100, 0102", manufacturer: "AXIS", model: "GR-ZB01-W", deviceJoinName: "AXIS Gear"
         //ClusterIDs: 0000 - Basic; 0004 - Groups; 0005 - Scenes; 0006 - On/Off; 0008 - Level Control; 0100 - Shade Configuration; 0102 - Window Covering;
         //Updated 2017-06-21
         //Updated 2017-08-24 - added power cluster 0001 - added battery, level, reporting, & health check
         //Updated 2018-01-04 - Axis Inversion & Increased Battery Reporting interval to 1 hour (previously 5 mins)
         //Updated 2018-01-08 - Updated battery conversion from [0-100 : 00 - 64] to [0-100 : 00-C8] to reflect firmware update
+        //Updated 2018-11-01 - added in configure reporting for refresh button, close when press on partial shade icon, update handler to parse between 0-254 as a percentage
     }
 }
+ 
 //Declare Clusters
 private getCLUSTER_POWER() {0x0001}
 private getCLUSTER_LEVEL() {0x0008}
@@ -39,6 +40,8 @@ def ShadesUp(){
 	}
     //sendEvent(name:"level", value:shadeValue, displayed:true)
     setLevel(shadeValue)
+	
+	setLevel(shadeValue)
 }
 
 //Custom command to decrement blind position by 25 %
@@ -52,17 +55,22 @@ def ShadesDown(){
 	}
     //sendEvent(name:"level", value:shadeValue, displayed:true)
     setLevel(shadeValue)
-	   
+	
+	setLevel(shadeValue)   
 }
 
 //Send Command through setLevel()
 def on() {
 	//sendEvent(name:"level", value:0, displayed:true)
-    setLevel(100)  
+    setLevel(100)
+	
+	setLevel(100)
 }
 
 //Send Command through setLevel()
 def off() {
+	setLevel(0)
+	
 	setLevel(0)
 }
 //Command to set the blind position (%) and log the event
@@ -71,15 +79,20 @@ def setLevel(value) {
     def L = Math.round(value);
     def i = Integer.valueOf(L.intValue());
     setWindowShade(i)
-	return zigbee.setLevel(i, 25)
+	zigbee.setLevel(i)
     
+	zigbee.setLevel(i)
 }
 //Send Command through setLevel()
 def open() {
-    setLevel(100)   
+    setLevel(100)
+	
+	setLevel(100)
 }
 //Send Command through setLevel()
 def close() {
+	setLevel(0)
+	
 	setLevel(0)
 }
 
@@ -105,17 +118,18 @@ def setWindowShade(value){
 //Refresh command
 def refresh() {
 	log.debug "parse() refresh"
-    return zigbee.readAttribute(CLUSTER_POWER, POWER_ATTR_BATTERY) +
+    return zigbee.configureReporting(CLUSTER_POWER, POWER_ATTR_BATTERY, 0x20, 1, 3600, 0x01) +
+           zigbee.configureReporting(CLUSTER_LEVEL, LEVEL_ATTR_LEVEL, 0x20, 1, 3600, 0x01) + 
+           zigbee.readAttribute(CLUSTER_POWER, POWER_ATTR_BATTERY) +
     	   zigbee.readAttribute(CLUSTER_LEVEL, LEVEL_ATTR_LEVEL)
-    	       
 }
 //configure reporting
 def configure() {
     log.debug "Configuring Reporting and Bindings."
-    sendEvent(name: "checkInterval", value: 3600, displayed: true, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
+    sendEvent(name: "checkInterval", value: 60, displayed: true, data: [protocol: "zigbee", hubHardwareId: device.hub.hardwareID])
     def cmds = 
-    	zigbee.configureReporting(CLUSTER_POWER, POWER_ATTR_BATTERY, 0x20, 3000, 3600, 0x01) +
-        zigbee.configureReporting(CLUSTER_LEVEL, LEVEL_ATTR_LEVEL, 0x20, 3000, 3600, 0x01)
+    	zigbee.configureReporting(CLUSTER_POWER, POWER_ATTR_BATTERY, 0x20, 1, 3600, 0x01) +
+        zigbee.configureReporting(CLUSTER_LEVEL, LEVEL_ATTR_LEVEL, 0x20, 1, 3600, 0x01)
         log.info "configure() --- cmds: $cmds"
     return refresh + cmds
 }
@@ -123,8 +137,11 @@ def configure() {
 def parse(String description) {
     log.trace "parse() --- description: $description"
 
+
     Map map = [:]
     if (description?.startsWith('read attr -')) {
+        map = parseReportAttributeMessage(description)
+    } else if (description?.startsWith('attr report -')) {
         map = parseReportAttributeMessage(description)
     }
 
@@ -148,9 +165,10 @@ private Map parseReportAttributeMessage(String description) {
     else if (descMap.clusterInt == CLUSTER_LEVEL && descMap.attrInt == LEVEL_ATTR_LEVEL) {
         resultMap.name = "level"
         def levelValue = Math.round(Integer.parseInt(descMap.value, 16))
+        def levelValuePercent = Math.round((levelValue/255)*100)
         //Set icon based on device feedback for the  open, closed, & partial configuration
-        setWindowShade(levelValue)
-        resultMap.value = levelValue 
+        setWindowShade(levelValuePercent)
+        resultMap.value = levelValuePercent
     }
     else {
         log.debug "parseReportAttributeMessage() --- ignoring attribute"
