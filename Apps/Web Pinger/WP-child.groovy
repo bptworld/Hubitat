a4a910b82cb1660c431ef3c7257d398106c193e6
bptworld <bryan@bptworld.com>
2019-01-15 16:25:36 -0500

    Update WP-child.groovy

--- Apps/Web Pinger/WP-child.groovy
+++ Apps/Web Pinger/WP-child.groovy
@@ -37,12 +37,15 @@
  *
  *  Changes:
  *
+ *  V1.0.2 - 01/15/19 - Updated footer with update check and links
  *  V1.0.1 - 01/10/19 - Tons of cosmetic changes. Added in Push option, time between polls. Changed it up to turn the switch off
- *			if website/internet becomes active again. Added in all the normal stuff - pause, enable/disable and debug
- *			logging.
+ *						if website/internet becomes active again. Added in all the normal stuff - pause, enable/disable and debug
+ *						logging.
  *  V1.0.0 - 01/09/19 - Hubitat Port of ST app 'SmartPing' - 2016 Jason Botello
  *
  */
+
+def version(){"v1.0.2"}
 
 definition(
 	name: "Web Pinger Child",
@@ -235,17 +238,6 @@ def pushNow(){
 	
 // Normal Stuff
 
-def getImage(type) {
-    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
-    if(type == "Blank") return "${loc}blank.png height=35 width=5}>"
-}
-
-def getFormat(type, myText=""){
-	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
-    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
-	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
-}
-
 def pauseOrNot(){
 	LOGDEBUG("In pauseOrNot...")
     state.pauseNow = pause1
@@ -287,6 +279,17 @@ def LOGDEBUG(txt){
     }
 }
 
+def getImage(type) {
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+}
+
+def getFormat(type, myText=""){
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
+}
+
 def display() {
 	section() {
 		paragraph getFormat("line")
@@ -294,9 +297,32 @@ def display() {
 	}
 }
 
-def display2() {
-	section() {
-		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>Web Pinger - App Version: 1.0.1 - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a></div>"
-	}
+def checkForUpdate(){
+	def params = [uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Web%20Pinger/version.json",
+				   	contentType: "application/json"]
+       	try {
+			httpGet(params) { response ->
+				def results = response.data
+				def appStatus
+				if(version() == results.currVersion){
+					appStatus = "${version()} - No Update Available - ${results.discussion}"
+				}
+				else {
+					appStatus = "<div style='color:#FF0000'>${version()} - Update Available (${results.currVersion})!</div><br>${results.parentRawCode}  ${results.childRawCode}  ${results.discussion}"
+					log.warn "${app.label} has an update available - Please consider updating."
+				}
+				return appStatus
+			}
+		} 
+        catch (e) {
+        	log.error "Error:  $e"
+    	}
 }
+
+def display2(){
+	section() {
+		def verUpdate = "${checkForUpdate()}"
+		paragraph getFormat("line")
+		paragraph "<div style='color:#1A77C9;text-align:center'>Web Pinger - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>${verUpdate}</div>"
+	}       
+} 
