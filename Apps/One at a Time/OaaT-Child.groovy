6accf6e036fd2ac9ceb0231d5ac9701ca21133f8
bptworld <bryan@bptworld.com>
2019-01-15 16:22:55 -0500

    Update OaaT-Child.groovy

--- Apps/One at a Time/OaaT-Child.groovy
+++ Apps/One at a Time/OaaT-Child.groovy
@@ -38,12 +38,15 @@
  *
  *  Changes:
  *
+ *  V1.1.2 - 01/15/19 - Updated footer with update check and links
  *  V1.1.1 - 12/30/18 - Updated to my new color theme.
  *  V1.1.0 - 12/10/18 - Changed over to Parent/Child type app. Create as many groups as you like. Also added in all the normal
  *						stuff: Enable/Disable switch, Pause switch and Logging options.
  *  V1.0.0 - 12/09/18 - Hubitat Port of ST app 'One at a Time Please' - 2015 Jody Albritton
  *
  */
+
+def version(){"v1.1.2"}
 
 definition(
     name: "One at a Time Child",
@@ -82,17 +85,6 @@ def pageConfig() {
 		}
 		display2()
 	}
-}
-
-def getImage(type) {
-    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
-    if(type == "Blank") return "${loc}blank.png height=35 width=5}>"
-}
-
-def getFormat(type, myText=""){
-	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
-    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
-	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
 }
 
 def installed() {
@@ -166,6 +158,17 @@ def LOGDEBUG(txt){
     }
 }
 
+def getImage(type) {
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+}
+
+def getFormat(type, myText=""){
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
+}
+
 def display() {
 	section() {
 		paragraph getFormat("line")
@@ -173,10 +176,34 @@ def display() {
 	}
 }
 
-def display2() {
-	section() {
-		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>One at a Time - App Version: 1.1.1 - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a></div>"
-	}
+def checkForUpdate(){
+	def params = [uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/One%20at%20a%20Time/version.json",
+				   	contentType: "application/json"]
+       	try {
+			httpGet(params) { response ->
+				def results = response.data
+				def appStatus
+				if(version() == results.currVersion){
+					appStatus = "${version()} - No Update Available - ${results.discussion}"
+				}
+				else {
+					appStatus = "<div style='color:#FF0000'>${version()} - Update Available (${results.currVersion})!</div><br>${results.parentRawCode}  ${results.childRawCode}  ${results.discussion}"
+					log.warn "${app.label} has an update available - Please consider updating."
+				}
+				return appStatus
+			}
+		} 
+        catch (e) {
+        	log.error "Error:  $e"
+    	}
 }
+
+def display2(){
+	section() {
+		def verUpdate = "${checkForUpdate()}"
+		paragraph getFormat("line")
+		paragraph "<div style='color:#1A77C9;text-align:center'>One at a Time - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>${verUpdate}</div>"
+	}       
+}  
+
 
