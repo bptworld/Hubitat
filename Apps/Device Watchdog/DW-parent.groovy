4aff81ebbf33ef8e7d29ebf5b0ebbed6a28c636c
bptworld <bryan@bptworld.com>
2019-01-15 15:10:51 -0500

    Update DW-parent.groovy

--- Apps/Device Watchdog/DW-parent.groovy
+++ Apps/Device Watchdog/DW-parent.groovy
@@ -36,6 +36,7 @@
  *
  *  Changes:
  *
+ *  V1.0.8 - 01/15/19 - Updated footer with update check and links
  *  V1.0.7 - 01/04/19 - Modification by rayzurbock. Report now shows 'battery level isn't reporting' when a device's battery
  *						attribute is null/blank/non-existent. Previously it showed 0.  Also adjusted the output on the Push report.
  *  V1.0.6 - 01/01/19 - Fixed typo in Pushover module.
@@ -49,6 +50,8 @@
  *  V1.0.0 - 12/21/18 - Initial release.
  *
  */
+
+def version(){"v1.0.8"}
 
 definition(
     name:"Device Watchdog",
@@ -118,7 +121,7 @@ def installCheck(){
 
 def getImage(type) {
     def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
-    if(type == "Blank") return "${loc}blank.png height=35 width=5}>"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
 }
 
 def getFormat(type, myText=""){
@@ -127,9 +130,32 @@ def getFormat(type, myText=""){
 	if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
 }
 
+def checkForUpdate(){
+	def params = [uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Device%20Watchdog/version.json",
+				   	contentType: "application/json"]
+       	try {
+			httpGet(params) { response ->
+				def results = response.data
+				def appStatus
+				if(version() == results.currVersion){
+					appStatus = "${version()} - No Update Available - ${results.discussion}"
+				}
+				else {
+					appStatus = "<div style='color:#FF0000'>${version()} - Update Available (${results.currVersion})!</div><br>${results.parentRawCode}  ${results.childRawCode}  ${results.discussion}"
+					log.warn "${app.label} has an update available - Please consider updating."
+				}
+				return appStatus
+			}
+		} 
+        catch (e) {
+        	log.error "Error:  $e"
+    	}
+}
+
 def display(){
 	section() {
+		def verUpdate = "${checkForUpdate()}"
 		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>Device Watchdog - App Version: 1.0.7 - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a></div>"
+		paragraph "<div style='color:#1A77C9;text-align:center'>Device Watchdog - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>${verUpdate}</div>"
 	}       
-}          
+}         
