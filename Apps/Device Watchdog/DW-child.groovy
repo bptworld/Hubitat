552aec48ddaf29bad517c02fb3ef1f716838bbf2
bptworld <bryan@bptworld.com>
2019-01-28 11:33:45 -0500

    Update DW-child.groovy

--- Apps/Device Watchdog/DW-child.groovy
+++ Apps/Device Watchdog/DW-child.groovy
@@ -36,6 +36,8 @@
  *
  *  Changes:
  *
+ *  V1.1.1 - 01/28/19 - Under the hood rewrite, better reporting. Also added NEW Device Watchdog Tile for use with dashboards
+ *  V1.1.0 - 01/25/19 - Added more wording regarding the 'all battery devices' switch
  *  V1.0.9 - 01/17/19 - Toggle switch added, Send or not to send Push notification when there is nothing to report.
  *  V1.0.8 - 01/15/19 - Updated footer with update check and links
  *  V1.0.7 - 01/04/19 - Modification by rayzurbock. Report now shows 'battery level isn't reporting' when a device's battery
@@ -52,7 +54,7 @@
  *
  */
 
-def version(){"v1.0.9"}
+def version(){"v1.1.1"}
 
 definition(
     name: "Device Watchdog Child",
@@ -78,14 +80,11 @@ def pageConfig() {
     display()
 		section("Instructions:", hideable: true, hidden: true) {
 			paragraph "<b>Notes:</b>"
-			paragraph "- Devices may show up in multiple lists but each device only needs to be selected once.<br>- Be sure to generate a new report before trying to view the 'Last Device Status Report'.<br>- All changes are saved right away, no need to exit out and back in before generating a new report."
+			paragraph "- Devices may show up in multiple lists but each device only needs to be selected once.<br>- All changes are saved right away, no need to exit out and back in before generating a new report."
+			paragraph "Also, if using the 'All Devices' toggle...If you add a new battery device after setting up this app, you will need to go back in to the app and toggle the 'all devices' switch off and then back on again to include the new device."
 		}
 		section(getFormat("header-green", "${getImage("Blank")}"+" Reports")) {
-			paragraph "<b>Click the button below to generate a new report. Be sure to watch your log to see when it finishes.<br>Usually only takes a minute or two depending on how many devices it's looking at.</b>"
-			paragraph "                        ", width:3
-			input "runButton", "button", title: "Click here to generate a new report.", width:5
-			paragraph "                        ", width:3
-			href "pageStatus", title: "Last Device Status Report", description: "Click here to view the Device Status Report."
+			href "pageStatus", title: "Device Status Report", description: "Click here to view the Device Status Report."
 		}
 		section(getFormat("header-green", "${getImage("Blank")}"+" Define whether this child app will be for checking Activity or Battery Levels")) {
 			input "triggerMode", "enum", required: true, title: "Select Trigger Type", submitOnChange: true,  options: ["Activity", "Battery_Level"]
@@ -94,7 +93,7 @@ def pageConfig() {
 				section(getFormat("header-green", "${getImage("Blank")}"+" Select your battery devices")) {
 					input(name: "allDevices", type: "bool", defaultValue: "false", title: "Select ALL battery devices?", submitOnChange: "true")
 					if(allDevices) {
-						paragraph "<b>** This will check all Battery device levels. **</b>"
+						paragraph "<b>** This will check all Battery device levels. **</b><br>If you add a new battery device after setting up this app, you will need to come back here and toggle the 'all battery devices' switch off and then back on again to include the new device."
 					} else {
 						input "batteryDevice", "capability.battery", title: "Select Battery Device(s)", submitOnChange: true, hideWhenEmpty: true, required: false, multiple: true
 					}
@@ -113,6 +112,16 @@ def pageConfig() {
 						paragraph "App will only display Devices BELOW Threshold."
 					}
 				}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Dashboard Tile")) {}
+		section("Instructions for Dashboard Tile:", hideable: true, hidden: true) {
+			paragraph "<b>Want to be able to view your data on a Dashboard? Now you can, simply follow these instructions!</b>"
+			paragraph " - Create a new 'Virtual Device'<br> - Name it something catchy like: 'Device Watchdog Tile'<br> - Use our 'Device Watchdog Tile' Driver<br> - Then select this new device below"
+			paragraph "Now all you have to do is add this device to one of your dashboards to see your counts on a tile!<br>Add a new tile with the following selections"
+			paragraph "- Pick a device = Device Watchdog Tile<br>- Pick a template = attribute<br>- 3rd box = watchdogActivity or watchdogBattery"
+			}
+		section() {
+			input(name: "watchdogTileDevice", type: "capability.actuator", title: "Vitual Device created to send the Data to:", submitOnChange: true, required: false, multiple: false)		
+		}
 				section(getFormat("header-green", "${getImage("Blank")}"+" General")) {label title: "Enter a name for this child app", required: false}
 				section() {
 					input(name: "enablerSwitch1", type: "capability.switch", title: "Enable/Disable child app with this switch - If Switch is ON then app is disabled, if Switch is OFF then app is active.", required: false, multiple: false)
@@ -159,6 +168,16 @@ def pageConfig() {
 					paragraph "App will only display INACTIVE Devices."
 				}
 		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Dashboard Tile")) {}
+		section("Instructions for Dashboard Tile:", hideable: true, hidden: true) {
+			paragraph "<b>Want to be able to view your data on a Dashboard? Now you can, simply follow these instructions!</b>"
+			paragraph " - Create a new 'Virtual Device'<br> - Name it something catchy like: 'Device Watchdog Tile'<br> - Use our 'Device Watchdog Tile' Driver<br> - Then select this new device below"
+			paragraph "Now all you have to do is add this device to one of your dashboards to see your counts on a tile!<br>Add a new tile with the following selections"
+			paragraph "- Pick a device = Device Watchdog Tile<br>- Pick a template = attribute<br>- 3rd box = watchdogActivity or watchdogBattery"
+			}
+		section() {
+			input(name: "watchdogTileDevice", type: "capability.actuator", title: "Vitual Device created to send the Data to:", submitOnChange: true, required: false, multiple: false)		
+		}
 		section(getFormat("header-green", "${getImage("Blank")}"+" General")) {label title: "Enter a name for this child app", required: false}
 		section() {
 			input(name: "enablerSwitch1", type: "capability.switch", title: "Enable/Disable child app with this switch - If Switch is ON then app is disabled, if Switch is OFF then app is active.", required: false, multiple: false)
@@ -171,52 +190,52 @@ def pageConfig() {
 
 def pageStatus(params) {
 	dynamicPage(name: "pageStatus", title: "Device Watchdog - Status", nextPage: null, install: false, uninstall: false, refreshInterval:0) {
+		clearMaps()
+		activityHandler()
+		log.warn("state.reportCount: ${state.reportCount} ***")
+		log.warn("state.timeSinceMap: ${state.timeSinceMap}")
 		if(triggerMode == "Battery_Level") {  // Battery
 			if(badORgood == false) {  // less than
-				section("Devices that have reported Battery levels less than $batteryThreshold") {}
-				if (state.batteryMap) {
-        			section() {
+				if(state.batteryMap) {
+        			section("Devices that have reported Battery levels less than $batteryThreshold") {
 						paragraph "${state.batteryMap}"
         			}
 				} else {
-					section("Oops!") { 
-						paragraph "Something went wrong. Please hit 'Done' and come back here again.<br>Still working on what causes this sometimes!"
+					section("Devices that have reported Battery levels less than $batteryThreshold") { 
+						paragraph "Nothing to report"
 					}
 				}
 			} else {  // more than
-				section("Devices with Battery reporting more than $batteryThreshold") {}
-				if (state.batteryMap) {
-        			section() {
+				if(state.batteryMap) {
+        			section("Devices with Battery reporting more than $batteryThreshold") {
 						paragraph "${state.batteryMap}"
         			}
 				} else {
-					section("Oops!") { 
-						paragraph "Something went wrong. Please hit 'Done' and come back here again.<br>Still working on what causes this sometimes!"
+					section("Devices with Battery reporting more than $batteryThreshold") { 
+						paragraph "Nothing to report"
 					}
 				}
 			}
 		}
 		if(triggerMode == "Activity") {
 			if(badORgood == false) {
-				section("Devices that have not reported in for $timeAllowed hour(s)") {}
-				if (state.timeSinceMap) {
-        			section() {
+				if(state.timeSinceMap) {
+        			section("Devices that have not reported in for $timeAllowed hour(s)") {
 						paragraph "${state.timeSinceMap}"
         			}
 				} else {
-					section("Oops!") {
-						paragraph "Something went wrong. Please hit 'Done' and come right back here again.<br>Still working on what causes this sometimes!"
-					}
+					section("Devices that have not reported in for $timeAllowed hour(s)") {
+						paragraph "Nothing to report"
+        			}
 				}
 			} else {
-				section("Devices that have reported in less than $timeAllowed hour(s)") {}
-				if (state.timeSinceMap) {
-        			section() {
+				if(state.timeSinceMap) {
+        			section("Devices that have reported in less than $timeAllowed hour(s)") {
 						paragraph "${state.timeSinceMap}"
         			}
 				} else {
-					section("Oops!") {
-						paragraph "Something went wrong. Please hit 'Done' and come right back here again.<br>Still working on what causes this sometimes!"
+					section("Devices that have reported in less than $timeAllowed hour(s)") {
+						paragraph "Nothing to report"
 					}
 				}
 			}
@@ -239,15 +258,24 @@ def updated() {
 def initialize() {
 	setDefaults()
 	if(triggerMode == "Activity") {
-		state.timeSinceMap = ""
-		state.timeSinceMapPhone = ""
 		schedule(timeToRun, activityHandler)
 	}
 	if(triggerMode == "Battery_Level") {
-		state.batteryMap = ""
-		state.batteryMapPhone = ""
 		if(allDevices) subscribe(location, "battery", activityHandler)
 		schedule(timeToRun, activityHandler)
+	}
+}
+
+def watchdogMapHandler(evt) {
+	if(triggerMode == "Activity") {
+		def watchdogActivityMap = "${state.timeSinceMap}"
+		LOGDEBUG("In watchdogMapHandler...Sending new Device Watchdog data to ${watchdogTileDevice}")
+    	watchdogTileDevice.sendWatchdogActivityMap(watchdogActivityMap)
+	}
+	if(triggerMode == "Battery_Level") {
+		def watchdogBatteryMap = "${state.batteryMap}"
+		LOGDEBUG("In watchdogMapHandler...Sending new Batterry Watchdog data to ${watchdogTileDevice}")
+    	watchdogTileDevice.sendWatchdogBatteryMap(watchdogBatteryMap)
 	}
 }
 
@@ -389,6 +417,7 @@ def activityHandler(evt) {
 	def rightNow = new Date()
 	if(triggerMode == "Activity") {state.timeSinceMap += "<br>Report generated: ${rightNow}<br>"}
 	if(triggerMode == "Battery_Level") {state.batteryMap += "<br>Report generated: ${rightNow}<br>"}
+	if(watchdogTileDevice) watchdogMapHandler()
 	if(sendPushMessage) pushNow()
 }	
 
@@ -424,6 +453,7 @@ def myBatteryHandler() {
 
 def mySensorHandler() {
 	log.info "     - - - - - Start (S) ${state.myType} - - - - -     "
+	state.reportCount = 0
 	LOGDEBUG("In mySensorHandler...")
 	state.mySensors.each { device ->
 		log.info "Working on... ${device}"
@@ -447,30 +477,41 @@ def mySensorHandler() {
 				log.info "${state.myType} - ${device} hasn't checked in since ${hour}h ${min}m ago."
 				state.timeSinceMap += "${state.myType} - ${device} hasn't checked in since ${hour}h ${min}m ago.<br>"
 				state.timeSinceMapPhone += "${device}-${hour}h ${min}m : "
+				state.reportCount = state.reportCount + 1
+				log.info "state.reportCount: ${state.reportCount}"
 			}
 		} else {
 			if(badORgood == true) {
 				log.info "${state.myType} - mySensors: ${device} last checked in ${hour}h ${min}m ago.<br>"
 				state.timeSinceMap += "${state.myType} - ${device} last checked in ${hour}h ${min}m ago.<br>"
 				state.timeSinceMapPhone += "${device}-${hour}h ${min}m : "
+				state.reportCount = state.reportCount + 1
+				log.info "state.reportCount: ${state.reportCount}"
 			}
 		}
 	}
 	log.info "     - - - - - End (S) ${state.myType} - - - - -     "
 }
 
-def appButtonHandler(btn){
-	LOGDEBUG("In appButtonHandler...")
-    state.btnCall = btn
-	LOGDEBUG("In appButtonHandler...state.btnCall: ${state.btnCall}")
-    if(state.btnCall == "runButton"){
-        log.info "${app.label} - Run Now button was pressed..."
-		state.timeSinceMap = ""
-		state.timeSinceMapPhone = ""
-		state.batteryMap = ""
-		state.batteryMapPhone = ""
-		runIn(1, activityHandler)
-    }
+def setupNewStuff() {
+	LOGDEBUG("In setupNewStuff...")
+	if(state.timeSinceMap == null) clearMaps()
+	if(state.timeSinceMapPhone == null) clearMaps()
+	if(state.batteryMap == null) clearMaps()
+	if(state.batteryMapPhone == null) clearMaps()
+	
+}
+	
+def clearMaps() {
+	state.timeSinceMap = [:]
+	state.timeSinceMapPhone = [:]
+	state.timeSinceMap = ""
+	state.timeSinceMapPhone = ""
+	
+	state.batteryMap = [:]
+	state.batteryMapPhone = [:]
+	state.batteryMap = ""
+	state.batteryMapPhone = ""
 }
 
 def pushNow(){
@@ -533,16 +574,14 @@ def pauseOrNot(){
 }
 
 def setDefaults(){
+	setupNewStuff()
     pauseOrNot()
     if(pause1 == null){pause1 = false}
     if(state.pauseApp == null){state.pauseApp = false}
 	if(logEnable == null){logEnable = false}
 	if(state.enablerSwitch2 == null){state.enablerSwitch2 = "off"}
-	if(state.timeSinceMap == null){state.timeSinceMap = ""}
-	if(state.timeSinceMapPhone == null){state.timeSinceMapPhone = ""}
-	if(state.batteryMap == null){state.batteryMap = ""}
-	if(state.batteryMapPhone == null){state.batteryMapPhone = ""}
 	if(pushAll == null){pushAll = false}
+	if(state.reportCount == null){state.reportCount = 0}
 }
 
 def logCheck(){
@@ -585,7 +624,7 @@ def checkForUpdate(){
 					appStatus = "${version()} - No Update Available - ${results.discussion}"
 				}
 				else {
-					appStatus = "<div style='color:#FF0000'>${version()} - Update Available (${results.currChildVersion})!</div><br>${results.parentRawCode}  ${results.childRawCode}  ${results.discussion}"
+					appStatus = "<div style='color:#FF0000'>${version()} - Update Available (${results.currChildVersion})!</div><br>${results.parentRawCode}  ${results.childRawCode}  ${results.driverRawCode}  ${results.discussion}"
 					log.warn "${app.label} has an update available - Please consider updating."
 				}
 				return appStatus
