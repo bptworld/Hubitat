3fd4719181572ff934c244bec4f981ee93d20462
bptworld <bryan@bptworld.com>
2019-04-16 13:41:16 -0400

    Update WH Driver.groovy

--- Apps/Welcome Home/WH Driver.groovy
+++ Apps/Welcome Home/WH Driver.groovy
@@ -15,7 +15,11 @@
  *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
  *
  *  Paypal at: https://paypal.me/bptworld
- *
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
  * ------------------------------------------------------------------------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -36,67 +40,68 @@
  *
  *  Changes:
  *
+ *  V1.0.2 - 04/16/19 - Code cleanup, added importUrl
+ *  V1.0.1 - 04/06/19 - deleted
  *  V1.0.0 - 02/09/19 - Initial release
  */
 
-def version(){"v1.0.0"}
+def version(){"v1.0.2"}
 
 metadata {
-	definition (name: "Global Variables Driver", namespace: "BPTWorld", author: "Bryan Turcotte") {
+	definition (name: "Global Variables Driver", namespace: "BPTWorld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Welcome%20Home/WH%20Driver.groovy") {
    		capability "Actuator"
 		
+		// Welcome Home
 		command "sendDataMap1", ["string"]
 		command "sendDataMap2", ["string"]
 		command "sendDataMap3", ["string"]
 		command "sendDataMap4", ["string"]
 		command "sendDataMap5", ["string"]
-
+		
 		attribute "globalBH1", "string"
 		attribute "globalBH2", "string"
 		attribute "globalBH3", "string"
 		attribute "globalBH4", "string"
 		attribute "globalBH5", "string"
+
+		attribute "globalIH1", "string"
+		attribute "globalIH2", "string"
+		attribute "globalIH3", "string"
+		attribute "globalIH4", "string"
+		attribute "globalIH5", "string"
 		
 	}
 	preferences() {    	
         section(){
-            input("debugMode", "bool", title: "Enable logging", required: false, defaultValue: false)
+            input("logEnable", "bool", title: "Enable logging", required: false, defaultValue: false)
         }
     }
 }
 
 // *** start global variables from Welcome Home ***
 def sendDataMap1(dataMap1) {
-    LOGDEBUG("In Welcome Home - sendDataMap1 - Received new data!")
+    if(logEnable) log.debug "In Welcome Home - sendDataMap1 - Received new data!"
 	def dMap1 = "${dataMap1}"
 	sendEvent(name: "globalBH1", value: dMap1, displayed: true)
 }
 def sendDataMap2(dataMap2) {
-    LOGDEBUG("In Welcome Home - sendDataMap2 - Received new data!")
+    if(logEnable) log.debug "In Welcome Home - sendDataMap2 - Received new data!"
 	def dMap2 = "${dataMap2}"
 	sendEvent(name: "globalBH2", value: dMap2, displayed: true)
 }
 def sendDataMap3(dataMap3) {
-    LOGDEBUG("In Welcome Home - sendDataMap3 - Received new data!")
+    if(logEnable) log.debug "In Welcome Home - sendDataMap3 - Received new data!"
 	def dMap3 = "${dataMap3}"
 	sendEvent(name: "globalBH3", value: dMap3, displayed: true)
 }
 def sendDataMap4(dataMap4) {
-    LOGDEBUG("In Welcome Home - sendDataMap4 - Received new data!")
+    if(logEnable) log.debug "In Welcome Home - sendDataMap4 - Received new data!"
 	def dMap4 = "${dataMap4}"
 	sendEvent(name: "globalBH4", value: dMap4, displayed: true)
 }
 def sendDataMap5(dataMap5) {
-    LOGDEBUG("In Welcome Home - sendDataMap5 - Received new data!")
+    if(logEnable) log.debug "In Welcome Home - sendDataMap5 - Received new data!"
 	def dMap5 = "${dataMap5}"
 	sendEvent(name: "globalBH5", value: dMap5, displayed: true)
 }
 //   *** End Welcome Home ***
-	
-def LOGDEBUG(txt) {
-    try {
-    	if (settings.debugMode) { log.debug("${txt}") }
-    } catch(ex) {
-    	log.error("LOGDEBUG unable to output requested data!")
-    }
-}
