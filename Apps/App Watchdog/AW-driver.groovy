317a9bfa2db322a2d97a04c8dad00e75c77ae735
bptworld <bryan@bptworld.com>
2019-04-15 20:51:22 -0400

    Update AW-driver.groovy

--- Apps/App Watchdog/AW-driver.groovy
+++ Apps/App Watchdog/AW-driver.groovy
@@ -13,7 +13,11 @@
  *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
  *
  *  Paypal at: https://paypal.me/bptworld
- *
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
  * ------------------------------------------------------------------------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -34,42 +38,53 @@
  *
  *  Changes:
  *
- *
+ *	V1.0.2 - 04/15/19 - Code cleanup, added importUrl
+ *  V1.0.1 - 02/08/19 - Messing around with attributes
  *  V1.0.0 - 02/07/19 - Initial release
  */
 
 metadata {
-	definition (name: "App Watchdog Tile", namespace: "BPTWorld", author: "Bryan Turcotte") {
+	definition (name: "App Watchdog Tile", namespace: "BPTWorld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/App%20Watchdog/AW-driver.groovy") {
    		capability "Actuator"
 
 		command "sendDataMap", ["string"]
+		command "sendAWinfo", ["string"]
 		
     	attribute "appVersions", "string"
+		attribute "app01", "string"
+		attribute "app02", "string"
+		attribute "app03", "string"
+		attribute "app04", "string"
+		attribute "app05", "string"
 		
 	}
 	preferences() {    	
         section(""){
 			input("fontSize", "text", title: "Font Size", required: true, defaultValue: "40")
-            input "debugMode", "bool", title: "Enable logging", required: true, defaultValue: true
+            input "logEnable", "bool", title: "Enable logging", required: true, defaultValue: true
         }
     }
 }
-	
-//received new data 
+
 def sendDataMap(dataMap) {
-    LOGDEBUG("In App Watchdog Tile - Received new data!")
-	state.versionDevice = "<table width='100%'><tr><td width='10'><td align='left'>"
-	state.versionDevice+= "<div style='line-height=50%;margin-top:0em;margin-bottom:0em;font-size:.${fontSize}em;'>${dataMap}</div>"
-	state.versionDevice+= "</td></tr></table>"
-	
-	sendEvent(name: "appVersions", value: state.versionDevice, displayed: true)
+    if(logEnable) log.debug "In App Watchdog Tile - Received new app data!"
+	state.appData = "<table width='100%'><tr><td width='10'><td align='left'>"
+	state.appData+= "<div style='line-height=50%;margin-top:0em;margin-bottom:0em;font-size:.${fontSize}em;'>${dataMap}</div>"
+	state.appData+= "</td></tr></table>"
+	sendEvent(name: "appVersions", value: state.appData, displayed: true)
 }
+
+def sendAWinfo(verMap) {
+    if(logEnable) log.debug "In App Watchdog Tile - Received new version data!"
+	if(logEnable) log.debug "In sendVersionMap..."
+	def newMap = "${verMap}"
+	def (appName, appVer) = newMap.split(':')
+	if(logEnable) log.debug "In sendVersionMap...appName: ${appName} - appVer: ${appVer}"
 	
-def LOGDEBUG(txt) {
-    try {
-    	if (settings.debugMode) { log.debug("${txt}") }
-    } catch(ex) {
-    	log.error("LOGDEBUG unable to output requested data!")
-    }
+	if(state.versionMap == null) state.versionMap = [:]
+	
+	state.versionMap.put(appName, appVer)
+	
+	sendEvent(name: "${theApp}", value: state.appVersion, displayed: true)
 }
 	
