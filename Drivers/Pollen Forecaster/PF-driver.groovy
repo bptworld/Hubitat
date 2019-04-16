57c0df9b05f5273ff1c8c2a5b9747040b3e342c3
bptworld <bryan@bptworld.com>
2019-04-16 13:32:17 -0400

    Update PF-driver.groovy

--- Drivers/Pollen Forecaster/PF-driver.groovy
+++ Drivers/Pollen Forecaster/PF-driver.groovy
@@ -13,7 +13,11 @@
  *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
  *
  *  Paypal at: https://paypal.me/bptworld
- *
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
  * ------------------------------------------------------------------------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -35,14 +39,15 @@
  *  Changes:
  *
  *
- * v2.0.0 - 04-10-2019 - Code cleanup. Added 'Tomorrow forecast', Updated display for Hubitat dashboard tile (@bptworld)
- * v1.0.0 - 12-10-2017 - Original ST 'Pollen Virtual Sensor' - Author: jschlackman (james@schlackman.org)
+ * V1.0.3 - 04/16/19 - Code cleanup, added importUrl
+ * v2.0.0 - 04/10/2019 - Code cleanup. Added 'Tomorrow forecast', Updated display for Hubitat dashboard tile (@bptworld)
+ * v1.0.0 - 12/10/2017 - Original ST 'Pollen Virtual Sensor' - Author: jschlackman (james@schlackman.org)
  *			ST version: https://github.com/jschlackman/PollenThing
  *
  */
 
 metadata {
-	definition (name: "Pollen Forecaster", namespace: "bptworld", author: "Bryan Turcotte") {
+	definition (name: "Pollen Forecaster", namespace: "bptworld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Drivers/Pollen%20Forecaster/PF-driver.groovy") {
 		capability "Sensor"
 		capability "Polling"
 
@@ -59,7 +64,7 @@ metadata {
 	}
 
 	preferences {
-		input name: "about", type: "paragraph", element: "paragraph", title: "Pollen Forecaster", description: "Retrieve data from Pollen.com. For use with Hubitat dashboards."
+		input name: "about", type: "paragraph", title: "Pollen Forecaster", description: "Retrieve data from Pollen.com. For use with Hubitat dashboards."
 		input name: "zipCode", type: "text", title: "Zip Code", required: true, defaultValue: "${location.zipCode}"
 		input "fontSizeIndex", "text", title: "Font Size-Index", required: true, defaultValue: "70"
 		input "fontSizeTriggers", "text", title: "Font Size-Triggers", required: true, defaultValue: "60"
