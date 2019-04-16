debd3c2c072ba35a07ab33966b778dca2bf31b49
bptworld <bryan@bptworld.com>
2019-04-16 13:37:33 -0400

    Update DW-driver.groovy

--- Apps/Device Watchdog/DW-driver.groovy
+++ Apps/Device Watchdog/DW-driver.groovy
@@ -13,7 +13,11 @@
  *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
  *
  *  Paypal at: https://paypal.me/bptworld
- *
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
  * ------------------------------------------------------------------------------------------------------------------------------
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -34,6 +38,7 @@
  *
  *  Changes:
  *
+ *  V1.0.4 - 04/16/19 - Cleanup Code, added importURL
  *  V1.0.3 - 03/31/19 - Added support for Status tiles
  *  V1.0.2 - 03/18/19 - Added support for mutiple tiles
  *  V1.0.1 - 02/25/19 - Added Device Status attribute
@@ -41,7 +46,7 @@
  */
 
 metadata {
-	definition (name: "Device Watchdog Tile", namespace: "BPTWorld", author: "Bryan Turcotte") {
+	definition (name: "Device Watchdog Tile", namespace: "BPTWorld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Device%20Watchdog/DW-driver.groovy") {
    		capability "Actuator"
 
 		command "sendWatchdogActivityMap1", ["string"]
@@ -83,19 +88,19 @@ metadata {
 	preferences() {    	
         section(""){
 			input("fontSize", "text", title: "Font Size", required: true, defaultValue: "40")
-            input "debugMode", "bool", title: "Enable logging", required: true, defaultValue: true
+            input "logEnable", "bool", title: "Enable logging", required: true, defaultValue: true
         }
     }
 }
 
 def sendWatchdogActivityMap1(activityMap1) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Activity data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Activity data!"
 	state.activityDevice1 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.activityDevice1+= "<div style='font-size:.${fontSize}em;'>${activityMap1}</div>"
 	state.activityDevice1+= "</td></tr></table>"
 	state.activityDevice1Count = state.activityDevice1.length()
 	if(state.activityDevice1Count <= 1000) {
-		LOGDEBUG("activityDevice1 - has ${state.activityDevice1Count} Characters<br>${state.activityDevice1}")
+		if(logEnable) log.debug "activityDevice1 - has ${state.activityDevice1Count} Characters<br>${state.activityDevice1}"
 	} else {
 		state.activityDevice1 = "Too many characters to display on Dashboard (${state.activityDevice1Count})"
 	}
@@ -103,13 +108,13 @@ def sendWatchdogActivityMap1(activityMap1) {
 }
 
 def sendWatchdogActivityMap2(activityMap2) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Activity data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Activity data!"
 	state.activityDevice2 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.activityDevice2+= "<div style='font-size:.${fontSize}em;'>${activityMap2}</div>"
 	state.activityDevice2+= "</td></tr></table>"
 	state.activityDevice2Count = state.activityDevice2.length()
 	if(state.activityDevice2Count <= 1000) {
-		LOGDEBUG("activityDevice2 - has ${state.activityDevice2Count} Characters<br>${state.activityDevice2}")
+		if(logEnable) log.debug "activityDevice2 - has ${state.activityDevice2Count} Characters<br>${state.activityDevice2}"
 	} else {
 		state.activityDevice2 = "Too many characters to display on Dashboard (${state.activityDevice2Count})"
 	}
@@ -117,13 +122,13 @@ def sendWatchdogActivityMap2(activityMap2) {
 }
 
 def sendWatchdogActivityMap3(activityMap3) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Activity data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Activity data!"
 	state.activityDevice3 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.activityDevice3+= "<div style='font-size:.${fontSize}em;'>${activityMap3}</div>"
 	state.activityDevice3+= "</td></tr></table>"
 	state.activityDevice3Count = state.activityDevice3.length()
 	if(state.activityDevice3Count <= 1000) {
-		LOGDEBUG("activityDevice3 - has ${state.activityDevice3Count} Characters<br>${state.activityDevice3}")
+		if(logEnable) log.debug "activityDevice3 - has ${state.activityDevice3Count} Characters<br>${state.activityDevice3}"
 	} else {
 		state.activityDevice3 = "Too many characters to display on Dashboard (${state.activityDevice3Count})"
 	}
@@ -131,13 +136,13 @@ def sendWatchdogActivityMap3(activityMap3) {
 }
 
 def sendWatchdogActivityMap4(activityMap4) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Activity data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Activity data!"
 	state.activityDevice4 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.activityDevice4+= "<div style='font-size:.${fontSize}em;'>${activityMap4}</div>"
 	state.activityDevice4+= "</td></tr></table>"
 	state.activityDevice4Count = state.activityDevice4.length()
 	if(state.activityDevice4Count <= 1000) {
-		LOGDEBUG("activityDevice4 - has ${state.activityDevice4Count} Characters<br>${state.activityDevice4}")
+		if(logEnable) log.debug "activityDevice4 - has ${state.activityDevice4Count} Characters<br>${state.activityDevice4}"
 	} else {
 		state.activityDevice4 = "Too many characters to display on Dashboard (${state.activityDevice4Count})"
 	}
@@ -145,13 +150,13 @@ def sendWatchdogActivityMap4(activityMap4) {
 }
 
 def sendWatchdogActivityMap5(activityMap5) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Activity data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Activity data!"
 	state.activityDevice5 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.activityDevice5+= "<div style='font-size:.${fontSize}em;'>${activityMap5}</div>"
 	state.activityDevice5+= "</td></tr></table>"
 	state.activityDevice5Count = state.activityDevice5.length()
 	if(state.activityDevice5Count <= 1000) {
-		LOGDEBUG("activityDevice5 - has ${state.activityDevice5Count} Characters<br>${state.activityDevice5}")
+		if(logEnable) log.debug "activityDevice5 - has ${state.activityDevice5Count} Characters<br>${state.activityDevice5}"
 	} else {
 		state.activityDevice5 = "Too many characters to display on Dashboard (${state.activityDevice5Count})"
 	}
@@ -159,13 +164,13 @@ def sendWatchdogActivityMap5(activityMap5) {
 }
 
 def sendWatchdogBatteryMap1(batteryMap1) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Battery data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Battery data!"
 	state.batteryDevice1 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.batteryDevice1+= "<div style='font-size:.${fontSize}em;'>${batteryMap1}</div>"
 	state.batteryDevice1+= "</td></tr></table>"
 	state.batteryDevice1Count = state.batteryDevice1.length()
 	if(state.batteryDevice1Count <= 1000) {
-		LOGDEBUG("batteryDevice1 - has ${state.batteryDevice1Count} Characters<br>${state.batteryDevice1}")
+		if(logEnable) log.debug "batteryDevice1 - has ${state.batteryDevice1Count} Characters<br>${state.batteryDevice1}"
 	} else {
 		state.batteryDevice1 = "Too many characters to display on Dashboard (${state.batteryDevice1Count})"
 	}
@@ -173,13 +178,13 @@ def sendWatchdogBatteryMap1(batteryMap1) {
 }
 
 def sendWatchdogBatteryMap2(batteryMap2) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Battery data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Battery data!"
 	state.batteryDevice2 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.batteryDevice2+= "<div style='font-size:.${fontSize}em;'>${batteryMap2}</div>"
 	state.batteryDevice2+= "</td></tr></table>"
 	state.batteryDevice2Count = state.batteryDevice2.length()
 	if(state.batteryDevice2Count <= 1000) {
-		LOGDEBUG("batteryDevice2 - has ${state.batteryDevice2Count} Characters<br>${state.batteryDevice2}")
+		if(logEnable) log.debug "batteryDevice2 - has ${state.batteryDevice2Count} Characters<br>${state.batteryDevice2}"
 	} else {
 		state.batteryDevice2 = "Too many characters to display on Dashboard (${state.batteryDevice2Count})"
 	}
@@ -187,13 +192,13 @@ def sendWatchdogBatteryMap2(batteryMap2) {
 }
 
 def sendWatchdogBatteryMap3(batteryMap3) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Battery data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Battery data!"
 	state.batteryDevice3 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.batteryDevice3+= "<div style='font-size:.${fontSize}em;'>${batteryMap3}</div>"
 	state.batteryDevice3+= "</td></tr></table>"
 	state.batteryDevice3Count = state.batteryDevice3.length()
 	if(state.batteryDevice3Count <= 1000) {
-		LOGDEBUG("batteryDevice3 - has ${state.batteryDevice3Count} Characters<br>${state.batteryDevice3}")
+		if(logEnable) log.debug "batteryDevice3 - has ${state.batteryDevice3Count} Characters<br>${state.batteryDevice3}"
 	} else {
 		state.batteryDevice3 = "Too many characters to display on Dashboard (${state.batteryDevice3Count})"
 	}
@@ -201,13 +206,13 @@ def sendWatchdogBatteryMap3(batteryMap3) {
 }
 
 def sendWatchdogBatteryMap4(batteryMap4) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Battery data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Battery data!"
 	state.batteryDevice4 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.batteryDevice4+= "<div style='font-size:.${fontSize}em;'>${batteryMap4}</div>"
 	state.batteryDevice4+= "</td></tr></table>"
 	state.batteryDevice4Count = state.batteryDevice4.length()
 	if(state.batteryDevice4Count <= 1000) {
-		LOGDEBUG("batteryDevice4 - has ${state.batteryDevice4Count} Characters<br>${state.batteryDevice4}")
+		if(logEnable) log.debug "batteryDevice4 - has ${state.batteryDevice4Count} Characters<br>${state.batteryDevice4}"
 	} else {
 		state.batteryDevice4 = "Too many characters to display on Dashboard (${state.batteryDevice4Count})"
 	}
@@ -215,13 +220,13 @@ def sendWatchdogBatteryMap4(batteryMap4) {
 }
 
 def sendWatchdogBatteryMap5(batteryMap5) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Battery data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Battery data!"
 	state.batteryDevice5 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.batteryDevice5+= "<div style='font-size:.${fontSize}em;'>${batteryMap5}</div>"
 	state.batteryDevice5+= "</td></tr></table>"
 	state.batteryDevice5Count = state.batteryDevice5.length()
 	if(state.batteryDevice5Count <= 1000) {
-		LOGDEBUG("batteryDevice5 - has ${state.batteryDevice5Count} Characters<br>${state.batteryDevice5}")
+		if(logEnable) log.debug "batteryDevice5 - has ${state.batteryDevice5Count} Characters<br>${state.batteryDevice5}"
 	} else {
 		state.batteryDevice5 = "Too many characters to display on Dashboard (${state.batteryDevice5Count})"
 	}
@@ -229,7 +234,7 @@ def sendWatchdogBatteryMap5(batteryMap5) {
 }
 
 def sendWatchdogStatusMap1(statusMap1) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Status data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Status data!"
 	state.statusDevice1 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.statusDevice1+= "<div style='font-size:.${fontSize}em;'>${statusMap1}</div>"
 	state.statusDevice1+= "</td></tr></table>"
@@ -237,7 +242,7 @@ def sendWatchdogStatusMap1(statusMap1) {
 }
 
 def sendWatchdogStatusMap2(statusMap2) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Status data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Status data!"
 	state.statusDevice2 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.statusDevice2+= "<div style='font-size:.${fontSize}em;'>${statusMap2}</div>"
 	state.statusDevice2+= "</td></tr></table>"
@@ -245,7 +250,7 @@ def sendWatchdogStatusMap2(statusMap2) {
 }
 
 def sendWatchdogStatusMap3(statusMap3) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Status data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Status data!"
 	state.statusDevice3 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.statusDevice3+= "<div style='font-size:.${fontSize}em;'>${statusMap3}</div>"
 	state.statusDevice3+= "</td></tr></table>"
@@ -253,7 +258,7 @@ def sendWatchdogStatusMap3(statusMap3) {
 }
 
 def sendWatchdogStatusMap4(statusMap4) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Status data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Status data!"
 	state.statusDevice4 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.statusDevice4+= "<div style='font-size:.${fontSize}em;'>${statusMap4}</div>"
 	state.statusDevice4+= "</td></tr></table>"
@@ -261,18 +266,10 @@ def sendWatchdogStatusMap4(statusMap4) {
 }
 
 def sendWatchdogStatusMap5(statusMap5) {
-    LOGDEBUG("In Device Watchdog Tile - Received new Status data!")
+    if(logEnable) log.debug "In Device Watchdog Tile - Received new Status data!"
 	state.statusDevice5 = "<table width='100%'><tr><td width='10'><td align='left'>"
 	state.statusDevice5+= "<div style='font-size:.${fontSize}em;'>${statusMap5}</div>"
 	state.statusDevice5+= "</td></tr></table>"
 	sendEvent(name: "watchdogStatus5", value: state.statusDevice5, displayed: true)
-}
-
-def LOGDEBUG(txt) {
-    try {
-    	if (settings.debugMode) { log.debug("${txt}") }
-    } catch(ex) {
-    	log.error("LOGDEBUG unable to output requested data!")
-    }
 }
 	
