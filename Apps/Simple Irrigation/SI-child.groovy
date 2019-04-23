0ad34d80ef7b64811318012c0d87e0eff4bb8fc2
bptworld <bryan@bptworld.com>
2019-04-23 13:21:34 -0400

    Create SI-child.groovy

---
+++ Apps/Simple Irrigation/SI-child.groovy
@@ -0,0 +1,276 @@
+/**
+ *  ****************  Simple Irrigation Child App  ****************
+ *
+ *  Design Usage:
+ *  For use with any valve device connected to your hose, like the Orbit Hose Water Timer. Features mutiple timers and
+ *  restrictions.
+ *
+ *  Copyright 2019 Bryan Turcotte (@bptworld)
+ * 
+ *  This App is free.  If you like and use this app, please be sure to give a shout out on the Hubitat forums to let
+ *  people know that it exists!  Thanks.
+ *
+ *  Remember...I am not a programmer, everything I do takes a lot of time and research!
+ *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
+ *
+ *  Paypal at: https://paypal.me/bptworld
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  If modifying this project, please keep the above header intact and add your comments/credits below - Thank you! -  @BPTWorld
+ *
+ *  App and Driver updates can be found at https://github.com/bptworld/Hubitat/
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  Changes:
+ *
+ *  V1.0.0 - 04/22/19 - Initial release.
+ *
+ */
+
+def setVersion() {
+	state.version = "v1.0.0"
+}
+
+definition(
+    name: "Simple Irrigation Child",
+    namespace: "BPTWorld",
+    author: "Bryan Turcotte",
+    description: "For use with any valve device connected to your hose, like the Orbit Hose Water Timer. Features mutiple timers and restrictions.",
+    category: "",
+	parent: "BPTWorld:Simple Irrigation",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "",
+    importUrl: "",
+)
+
+preferences {
+    page(name: "pageConfig")
+}
+
+def pageConfig() {
+    dynamicPage(name: "", title: "<h2 style='color:#1A77C9;font-weight: bold'>Simple Irrigation</h2>", install: true, uninstall: true, refreshInterval:0) {
+		display() 
+        section("Instructions:", hideable: true, hidden: true) {
+			paragraph "<b>Notes:</b>"
+    		paragraph "For use with any valve device connected to your hose, like the Orbit Hose Water Timer. Features mutiple timers and restrictions."
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Valve Devices")) {
+			input "device", "capability.valve", title: "Select Valve Device", required: true	
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Schedule")) {
+			input(name: "days", type: "enum", title: "Only water on these days", description: "Days to water", required: true, multiple: true, options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+			paragraph "Select up to 3 watering sessions per day."
+			input "startTime1", "time", title: "Time to turn on 1", required: true, width: 6
+        	input "onLength1", "number", title: "Leave valve on for how long (in minutes)", required: true, width: 6
+			paragraph "<hr>"
+			input "startTime2", "time", title: "Time to turn on 2", required: false, width: 6
+        	input "onLength2", "number", title: "Leave valve on for how long (in minutes)", required: false, width: 6
+			paragraph "<hr>"
+			input "startTime3", "time", title: "Time to turn on 3", required: false, width: 6
+        	input "onLength3", "number", title: "Leave valve on for how long (in minutes)", required: false, width: 6
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Check the Weather")) {
+			paragraph "Simple Irrigation can be disabled using any 'Switch' type device. I highly recommend using WATO to turn a virtual switch on/off based on any device parameter."
+			paragraph "If ANY of the options below are ON, watering will be cancelled."
+			input "rainSensor", "capability.switch", title: "Rain Switch", required: false
+			input "windSensor", "capability.switch", title: "Wind Switch", required: false
+			input "otherSensor", "capability.switch", title: "Other Switch", required: false
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" General")) {label title: "Enter a name for this automation", required: false}
+        section() {
+            input(name: "logEnable", type: "bool", defaultValue: "true", title: "Enable Debug Logging", description: "Enable extra logging for debugging.")
+		}
+		display2()
+	}
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {	
+    if(logEnable) log.debug "Updated with settings: ${settings}"
+	unschedule()
+	initialize()
+}
+
+def initialize() {
+    setDefaults()
+	if(startTime1) schedule(startTime1, turnValveOn1)
+	if(startTime2) schedule(startTime2, turnValveOn2)
+	if(startTime3) schedule(startTime3, turnValveOn3)
+}
+	
+def turnValveOn1() {
+	state.valveStatus = device.currentValue("valve")
+	dayOfTheWeekHandler()
+	checkForWeather()
+	if(state.daysMatch == "yes") {
+		if(state.canWater == "yes") {
+			if(logEnable) log.debug "In turnValveOn1..."
+			if(state.valveStatus == "closed") {
+				if(logEnable) log.debug "In turnValveOn1 - trying to open - will check again in 10 seconds"
+				valveDevice.open()
+    			runIn(10, turnValveOn1)		// Repeat for safety
+			} else {
+				def delay = onLength1 * 60
+				if(logEnable) log.debug "In turnValveOn1 - Valve is now ${state.valveStatus}, Setting valve timer to off in ${onLength1} minutes"
+				log.warn "${device} is now ${state.valveStatus}"
+				runIn(delay, turnValveOff)
+			}
+		} else {
+			log.info "${app.label} didn't pass weather check. Water not turned on."
+			turnValveOff()
+		}
+	} else {
+		log.info "${app.label} didn't pass days check. Water not turned on."
+		turnValveOff()
+	}	
+}
+
+def turnValveOn2() {
+	state.valveStatus = device.currentValue("valve")
+	checkForWeather()
+	if(state.canWater == "yes") {
+		if(logEnable) log.debug "In turnValveOn2..."
+		if(state.valveStatus == "closed") {
+			if(logEnable) log.debug "In turnValveOn2 - trying to open - will check again in 10 seconds"
+			valveDevice.open()
+    		runIn(10, turnValveOn2)		// Repeat for safety
+		} else {
+			def delay = onLength2 * 60
+			if(logEnable) log.debug "In turnValveOn2 - Valve is now ${state.valveStatus}, Setting valve timer to off in ${onLength2} minutes"
+			log.warn "${device} is now ${state.valveStatus}"
+			runIn(delay, turnValveOff)
+		}
+	} else {
+		log.info "${app.label} didn't pass weather check. Water not turned on."
+		turnValveOff()
+	}	
+}
+
+def turnValveOn3() {
+	state.valveStatus = device.currentValue("valve")
+	checkForWeather()
+	if(state.canWater == "yes") {
+		if(logEnable) log.debug "In turnValveOn3..."
+		if(state.valveStatus == "closed") {
+			if(logEnable) log.debug "In turnValveOn3 - trying to open - will check again in 10 seconds"
+			valveDevice.open()
+    		runIn(10, turnValveOn3)		// Repeat for safety
+		} else {
+			def delay = onLength3 * 60
+			if(logEnable) log.debug "In turnValveOn3 - Valve is now ${state.valveStatus}, Setting valve timer to off in ${onLength3} minutes"
+			log.warn "${device} is now ${state.valveStatus}"
+			runIn(delay, turnValveOff)
+		}
+	} else {
+		log.info "${app.label} didn't pass weather check. Water not turned on."
+		turnValveOff()
+	}	
+}
+
+def turnValveOff() {
+	state.valveStatus = device.currentValue("valve")
+	if(logEnable) log.debug "In turnValveOff..."
+    if(state.valveStatus == "open") {
+		if(logEnable) log.debug "In turnValveOff - trying to close - will check again in 10 seconds"
+		valveDevice.close()
+    	runIn(10, turnValveOff)		// Repeat for safety
+	} else {
+		if(logEnable) log.debug "In turnValveOff - Valve is now ${state.valveStatus}"
+		log.warn "${device} is now ${state.valveStatus}"
+	}
+}
+
+def checkForWeather() {
+	if(logEnable) log.debug "In checkForWeather..."
+	if(rainSensor) state.rainDevice = rainSensor.currentValue("switch")
+	if(windSensor) state.windDevice = windSensor.currentValue("switch")
+	if(otherSensor) state.otherDevice = otherSensor.currentValue("switch")
+	if(state.rainDevice == "on" || state.windDevice == "on" || state.otherDevice == "on") {
+		if(logEnable) log.debug "In checkForWeather - Weather Check failed."
+		state.canWater = "no"
+	} else {
+		if(logEnable) log.debug "In checkForWeather - Weather Check passed."
+		state.canWater = "yes"
+	}
+}
+
+def dayOfTheWeekHandler() {
+	if(logEnable) log.debug "In dayOfTheWeek..."
+	Calendar date = Calendar.getInstance()
+	int dayOfTheWeek = date.get(Calendar.DAY_OF_WEEK)
+	if(dayOfTheWeek == 1) state.dotWeek = "Sunday"
+	if(dayOfTheWeek == 2) state.dotWeek = "Monday"
+	if(dayOfTheWeek == 3) state.dotWeek = "Tuesday"
+	if(dayOfTheWeek == 4) state.dotWeek = "Wednesday"
+	if(dayOfTheWeek == 5) state.dotWeek = "Thursday"
+	if(dayOfTheWeek == 6) state.dotWeek = "Friday"
+	if(dayOfTheWeek == 7) state.dotWeek = "Saturday"
+
+	if(days.contains(state.dotWeek)) {
+		if(logEnable) log.debug "In dayOfTheWeekHandler - Days of the Week Passed"
+		state.daysMatch = "yes"
+	} else {
+		if(logEnable) log.debug "In dayOfTheWeekHandler - Days of the Week Check Failed"
+		state.daysMatch = "no"
+	}
+}
+
+// ********** Normal Stuff **********
+
+def setDefaults(){
+    if(pauseApp == null){pauseApp = false}
+	if(logEnable == null){logEnable = false}
+	if(state.rainDevice == null){state.rainDevice = "off"}
+	if(state.windDevice == null){state.windDevice = "off"}
+	if(state.otherDevice == null){state.otherDevice = "off"}
+	if(state.daysMatch == null){state.daysMatch = "no"}
+}
+
+def getImage(type) {					// Modified from @Stephack Code
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+}
+
+def getFormat(type, myText=""){			// Modified from @Stephack Code
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
+}
+
+def display() {
+	section() {
+		paragraph getFormat("line")
+		input "pauseApp", "bool", title: "Pause App", required: true, submitOnChange: true, defaultValue: false
+		if(pauseApp) {paragraph "<font color='red'>App is Paused</font>"}
+		if(!pauseApp) {paragraph "App is not Paused"}
+	}
+}
+
+def display2(){
+	setVersion()
+	section() {
+		paragraph getFormat("line")
+		paragraph "<div style='color:#1A77C9;text-align:center'>Simple Irrigation - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>Get app update notifications and more with <a href='https://github.com/bptworld/Hubitat/tree/master/Apps/App%20Watchdog' target='_blank'>App Watchdog</a><br>${state.version}</div>"
+	}       
+}
