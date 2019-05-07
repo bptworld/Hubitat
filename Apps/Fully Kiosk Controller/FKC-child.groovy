8ef3075d32506c0d48a686a0335c5160637292c4
bptworld <bryan@bptworld.com>
2019-05-07 18:53:20 -0400

    Update FKC-child.groovy

--- Apps/Fully Kiosk Controller/FKC-child.groovy
+++ Apps/Fully Kiosk Controller/FKC-child.groovy
@@ -51,7 +51,7 @@ definition(
     iconUrl: "",
     iconX2Url: "",
     iconX3Url: "",
-	importUrl: "",
+	importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Fully%20Kiosk%20Controller/FKC-child.groovy",
 )
 
 preferences {
