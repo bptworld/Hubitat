6768a5270dc8f9b4c9f280805ae93cfad68354df
bptworld <bryan@bptworld.com>
2019-07-11 13:33:28 -0400

    Update L-driver.groovy

--- Ported/Life360/L-driver.groovy
+++ Ported/Life360/L-driver.groovy
@@ -28,6 +28,7 @@
  *
  * ---- End of Original Header ----
  *
+ *  V1.0.7 - 07/10/19 - Added code for dashboard tiles, Info and Places tiles
  *  V1.0.6 - 07/10/19 - Added a Dashboard Tile
  *  V1.0,5 - 07/08/19 - Added Avatar and code cleanup (cwwilson08)
  *  V1.0.4 - 07/07/19 - Removed clientID field, no longer needed
@@ -42,8 +43,13 @@ import java.text.SimpleDateFormat
 preferences {
 	input title:"Distance", description:"This feature allows you change the display of distance to either Miles or KM. Please note, any changes will take effect only on the NEXT update or forced refresh.", type:"paragraph", element:"paragraph"
 	input name: "units", type: "enum", title: "Distance Units", description: "Miles or Kilometers", required: false, options:["Kilometers","Miles"]
-    input("fontSize", "text", title: "Data Font Size", required: true, defaultValue: "15")
+    input "life360Paid", "bool", title: "Version of Life360 (off=Free, on=Paid)", required: true, defaultValue: false
+    input("avatarFontSize", "text", title: "Avatar Font Size", required: true, defaultValue: "15")
     input("avatarSize", "text", title: "Avatar Size by Percentage", required: true, defaultValue: "75")
+    
+    input("numOfLines", "number", title: "How many lines to display on History Tile (from 1 to 10 only)", required:true, defaultValue: 5)
+    input("historyFontSize", "text", title: "History Font Size", required: true, defaultValue: "15")
+    input("historyHourType", "bool", title: "Time Selection for History Tile (Off for 24h, On for 12h)", required: false, defaultValue: false)
     input "logEnable", "bool", title: "Enable logging", required: true, defaultValue: false
 } 
  
@@ -79,13 +85,15 @@ metadata {
     attribute "avatar", "string"
     attribute "avatarHtml", "string"
     attribute "life360Tile1", "string"
+    attribute "history", "string"
 
 	command "refresh"
 	command "asleep"
     command "awake"
     command "toggleSleeping"
     command "setBattery",["number","boolean"]
-        
+    command "sendHistory", ["string"]
+    command "historyClearData"
 	}
 
 	simulator {
@@ -95,13 +103,28 @@ metadata {
 }
 
 def sendLife360Tile1() {
-    if(logEnable) log.debug "in Life360 User ... Making the Tile"
+    if(logEnable) log.debug "in Life360 User ... Making the Avatar Tile"
     def avat = device.currentValue('avatar')
     def add1 = device.currentValue('address1')
     def unit = device.currentValue('units')
     def bThere = device.currentValue('since')
     def bLevel = device.currentValue('battery')
     def bCharge = device.currentValue('powerSource')
+    def bSpeedKm = device.currentValue('speedKm')
+    def bSpeedMiles = device.currentValue('speedMiles')
+    if(unit == "Kilometers") {
+        bUnitsa = "${bSpeedKm} KMH"
+    } else {
+        bUnitsa = "${bSpeedMiles} MPH"
+    }
+    
+    def binTransit = device.currentValue('inTransit')
+    if(binTransit == "true") {
+        binTransita = "Moving"
+    } else {
+        binTransita = "Not Moving"
+    }
+    
     def bWifi = device.currentValue('wifiState')
     if(bWifi == "true") {
         bWifiS = "Wifi"
@@ -116,10 +139,18 @@ def sendLife360Tile1() {
     SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("E dd-MM @ hh:mm a")
     String dateSince = DATE_FORMAT.format(theDate)
     
-	tileMap = "<table width='100%' valign='top'>"
-    tileMap += "<tr><td width='25%'><img src='${avat}' height='${avatarSize}%'></td>"
-    tileMap += "<td width='75%'>At: ${add1}<br>Since: ${dateSince}<br>${state.status}<br>Phone Lvl: ${bLevel} - ${bCharge} - ${bWifiS}</td>"
-    tileMap += "</tr></table>"
+    if(life360Paid) {
+	    tileMap = "<table width='100%' valign='top'>"
+        tileMap += "<tr><td width='25%'><img src='${avat}' height='${avatarSize}%'></td>"
+        tileMap += "<td width='75%'><p style='font-size:${avatarFontSize}px'>At: ${add1}<br>Since: ${dateSince}<br>${state.status}<br>${binTransita} - ${bUnitsa}<br>Phone Lvl: ${bLevel} - ${bCharge} - ${bWifiS}</p></td>"
+        tileMap += "</tr></table>"
+    } else {  // Free
+        tileMap = "<table width='100%' valign='top'>"
+        tileMap += "<tr><td width='25%'><img src='${avat}' height='${avatarSize}%'></td>"
+        tileMap += "<td width='75%'><p style='font-size:${avatarFontSize}px'>At: ${add1}<br>Since: ${dateSince}<br>${state.status}</p></td>"
+        tileMap += "</tr></table>"
+    }
+    
 	state.tileDevice1Count = tileMap.length()
 	if(state.tileDevice1Count <= 1000) {
 		if(logEnable) log.debug "tileMap - has ${state.tileDevice1Count} Characters<br>${tileMap}"
@@ -128,6 +159,162 @@ def sendLife360Tile1() {
 	}
 	sendEvent(name: "life360Tile1", value: tileMap, displayed: true)
 }
+
+def sendHistory(historyLog) {
+    if(logEnable) log.debug "In Life360 User - Making the History Tile"
+	if(logEnable) log.debug "Life360 User - New item - ${historyLog}"
+    
+    def hMessage = "${historyLog}"
+	
+	// Read in the maps
+	try {
+		sOne = state.speechMap1.get(state.s,nMessage)
+		sTwo = state.speechMap2.get(state.s,nMessage)
+		sThree = state.speechMap3.get(state.s,nMessage)
+		sFour = state.speechMap4.get(state.s,nMessage)
+		sFive = state.speechMap5.get(state.s,nMessage)
+		sSix = state.speechMap6.get(state.s,nMessage)
+		sSeven = state.speechMap7.get(state.s,nMessage)
+		sEight = state.speechMap8.get(state.s,nMessage)
+		sNine = state.speechMap9.get(state.s,nMessage)
+		sTen = state.speechMap10.get(state.s,nMessage)
+	}
+	catch (e) {
+        //log.error "Error:  $e"
+    }
+	
+	if(logEnable) log.debug "What Did I Say - OLD -<br>sOne: ${sOne}<br>sTwo: ${sTwo}<br>sThree: ${sThree}<br>sFour: ${sFour}<br>sFive: ${sFive}"
+	
+	if(sOne == null) sOne = "${state.nMessage}"
+	if(sTwo == null) sTwo = "${state.nMessage}"
+	if(sThree == null) sThree = "${state.nMessage}"
+	if(sFour == null) sFour = "${state.nMessage}"
+	if(sFive == null) sFive = "${state.nMessage}"
+	if(sSix == null) sSix = "${state.nMessage}"
+	if(sSeven == null) sSeven = "${state.nMessage}"
+	if(sEight == null) sEight = "${state.nMessage}"
+	if(sNine == null) sNine = "${state.nMessage}"
+	if(sTen == null) sTen = "${state.nMessage}"
+	
+	// Move all messages down 1 slot
+	mTen = sNine
+	mNine = sEight
+	mEight = sSeven
+	mSeven = sSix
+	mSix = sFive
+	mFive = sFour
+	mFour = sThree
+	mThree = sTwo
+	mTwo = sOne
+	
+	getDateTime()
+    mOne = "${state.newdate} - ${hMessage}"
+	
+	if(logEnable) log.debug "What Did I Say - NEW -<br>mOne: ${mOne}<br>mTwo: ${mTwo}<br>mThree: ${mThree}<br>mFour: ${mFour}<br>mFive: ${mFive}"
+	
+	// Fill the maps back in
+	try {
+		state.speechMap1.put(state.s,mOne)
+		state.speechMap2.put(state.s,mTwo)
+		state.speechMap3.put(state.s,mThree)
+		state.speechMap4.put(state.s,mFour)
+		state.speechMap5.put(state.s,mFive)
+		state.speechMap6.put(state.s,mSix)
+		state.speechMap7.put(state.s,mSeven)
+		state.speechMap8.put(state.s,mEight)
+		state.speechMap9.put(state.s,mNine)
+		state.speechMap10.put(state.s,mTen)
+	}
+	catch (e) {
+        //log.error "Error:  $e"
+    }
+	
+	historyLog = "<table width='100%'><tr><td align='left'>"
+	if(numOfLines == 1) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}</p>"
+	}
+	if(numOfLines == 2) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}</p>"
+	}
+	if(numOfLines == 3) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}</p>"
+	}
+	if(numOfLines == 4) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}</p>"
+	}
+	if(numOfLines == 5) {
+        historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}</p>"
+	} 
+	if(numOfLines == 6) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}<br>${mSix}</p>"
+	}
+	if(numOfLines == 7) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}<br>${mSix}<br>${mSeven}</p>"
+	}
+	if(numOfLines == 8) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}<br>${mSix}<br>${mSeven}<br>${mEight}</p>"
+	}
+	if(numOfLines == 9) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}<br>${mSix}<br>${mSeven}<br>${mEight}<br>${mNine}</p>"
+	}
+	if(numOfLines == 10) {
+		historyLog+= "<p style='font-size:${historyFontSize}px'>${mOne}<br>${mTwo}<br>${mThree}<br>${mFour}<br>${mFive}<br>${mSix}<br>${mSeven}<br>${mEight}<br>${mNine}<br>${mTen}</p"
+	}
+	historyLog+= "</td></tr></table>"
+	
+	state.historyTileCount = historyLog.length()
+	if(state.historyTileCount <= 1000) {
+		if(logEnable) log.debug "Life360 History - ${state.historyTileCount} Characters<br>${historyLog}"
+	} else {
+		historyLog = "Too many characters to display on Dashboard"
+	}
+    if(logEnable) log.debug "History Log: ${historyLog}"
+	sendEvent(name: "history", value: historyLog, displayed: true)
+}
+
+def installed(){
+    log.info "Life360 User Installed"
+    historyClearData()
+}
+
+def updated() {
+    log.info "Life360 User has been Updated"
+}
+
+def getDateTime() {
+	def date = new Date()
+	if(historyHourType == false) state.newdate=date.format("MM-d HH:mm")
+	if(historyHourType == true) state.newdate=date.format("MM-d hh:mm")
+}
+
+def historyClearData(){
+	if(logEnable) log.debug "Life360 User - clearing the data"
+	state.nMessage = "No Data"
+	state.s = "s"
+	state.speechMap1 = [:]
+	state.speechMap1.put(state.s,state.nMessage)
+	state.speechMap2 = [:]
+	state.speechMap2.put(state.s,state.nMessage)
+	state.speechMap3 = [:]
+	state.speechMap3.put(state.s,state.nMessage)
+	state.speechMap4 = [:]
+	state.speechMap4.put(state.s,state.nMessage)
+	state.speechMap5 = [:]
+	state.speechMap5.put(state.s,state.nMessage)
+	state.speechMap6 = [:]
+	state.speechMap6.put(state.s,state.nMessage)
+	state.speechMap7 = [:]
+	state.speechMap7.put(state.s,state.nMessage)
+	state.speechMap8 = [:]
+	state.speechMap8.put(state.s,state.nMessage)
+	state.speechMap9 = [:]
+	state.speechMap9.put(state.s,state.nMessage)
+	state.speechMap10 = [:]
+	state.speechMap10.put(state.s,state.nMessage)
+	
+	historyLog = "Waiting for Data..."
+	sendEvent(name: "history", value: historyLog, displayed: true)
+}	
 
 def generatePresenceEvent(boolean present, homeDistance) {
 	if(logEnable) log.debug "Life360 generatePresenceEvent (present = $present, homeDistance = $homeDistance)"
