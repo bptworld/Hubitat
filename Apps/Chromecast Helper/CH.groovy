68730586190a3c9d98e31c9847df7a198188f924
bptworld <bryan@bptworld.com>
2019-07-21 08:21:55 -0400

    Create CH.groovy

---
+++ Apps/Chromecast Helper/CH.groovy
@@ -0,0 +1,139 @@
+/**
+ *  **************** Chromecast Helper App  ****************
+ *
+ *  Design Usage:
+ *  Simple way to help keep your Goolge/Nest devices alive.
+ *  Chromecast Integration (beta) is required for use of this app.
+ *
+ *  Copyright 2019 Bryan Turcotte (@bptworld)
+ * 
+ *  This App is free.  If you like and use this app, please be sure to give a shout out on the Hubitat forums to let
+ *  people know that it exists!  Thanks.
+ *
+ *  Remember...I am not a programmer, everything I do takes a lot of time and research!
+ *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
+ *
+ *  Paypal at: https://paypal.me/bptworld
+ * 
+ *  Unless noted in the code, ALL code contained within this app is mine. You are free to change, ripout, copy, modify or
+ *  otherwise use the code in anyway you want. This is a hobby, I'm more than happy to share what I have learned and help
+ *  the community grow. Have FUN with it!
+ * 
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  If modifying this project, please keep the above header intact and add your comments/credits below - Thank you! -  @BPTWorld
+ *
+ *  App and Driver updates can be found at https://github.com/bptworld/Hubitat/
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  Changes:
+ *
+ *  V1.0.0 - 07/21/19 - Initial release.
+ *
+ */
+
+def setVersion() {
+	state.version = "v1.0.0"
+}
+
+definition(
+    name: "Chromecast Helper",
+    namespace: "BPTWorld",
+    author: "Bryan Turcotte",
+    description: "Simple way to help keep your Goolge/Nest devices alive.",
+    category: "Convenience",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "",
+	importUrl: "",
+)
+
+preferences {
+    page(name: "pageConfig")
+}
+
+def pageConfig() {
+    dynamicPage(name: "", title: "<h2 style='color:#1A77C9;font-weight: bold'>Chromecast Helper</h2>", install: true, uninstall: true, refreshInterval:0) {
+		display() 
+        section("Instructions:", hideable: true, hidden: true) {
+			paragraph "<b>Notes:</b>"
+    		paragraph "Simple way to help keep your Goolge/Nest devices alive."
+            paragraph "* Chromecast Integration (beta) is required for use of this app."
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" Options")) {
+			paragraph "This will send an 'initialize' every X minutes to help keep your Google/Nest speakers alive."
+			input "speaker", "capability.speechSynthesis", title: "Choose your Google/Nest speaker(s)", required: true, multiple: true
+            input "gInitRepeat", "number", title: "Initialize Google/Nest devices every X minutes? (recommended: 4)", required: true
+		}
+		section(getFormat("header-green", "${getImage("Blank")}"+" General")) {label title: "Enter a name for this automation", required: false}
+        section() {
+            input(name: "logEnable", type: "bool", defaultValue: "true", title: "Enable Debug Logging", description: "Enable extra logging for debugging.")
+		}
+		display2()
+	}
+}
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+def updated() {	
+    if(logEnable) log.debug "Updated with settings: ${settings}"
+	unschedule()
+	initialize()
+}
+
+def initialize() {
+    setDefaults()
+	runIn(gInitRepeat,initializeSpeaker)
+}
+
+def initializeSpeaker() {
+	if(logEnable) log.debug "In initializeSpeaker - Initializing ${speaker}"
+	speaker.initialize()
+	repeat = gInitRepeat * 60
+	runIn(repeat,initializeSpeaker)
+}
+    
+// ********** Normal Stuff **********
+
+def setDefaults(){
+	if(logEnable == null){logEnable = false}
+}
+
+def getImage(type) {					// Modified from @Stephack Code
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+}
+
+def getFormat(type, myText=""){			// Modified from @Stephack Code
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<div style='color:blue;font-weight: bold'>${myText}</div>"
+}
+
+def display() {
+	section() {
+		paragraph getFormat("line")
+	}
+}
+
+def display2(){
+	setVersion()
+	section() {
+		paragraph getFormat("line")
+		paragraph "<div style='color:#1A77C9;text-align:center'>Chromecast Helper - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>Get app update notifications and more with <a href='https://github.com/bptworld/Hubitat/tree/master/Apps/App%20Watchdog' target='_blank'>App Watchdog</a><br>${state.version}</div>"
+	}       
+}
