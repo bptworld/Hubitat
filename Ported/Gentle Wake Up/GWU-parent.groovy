04cf1bdb022108d38a03256d40ffbbb50480407b
bptworld <bryan@bptworld.com>
2019-08-08 11:51:17 -0400

    Create GWU-parent.groovy

---
+++ Ported/Gentle Wake Up/GWU-parent.groovy
@@ -0,0 +1,130 @@
+/**
+ *  **************** Gentle Wake Up Parent ****************
+ *
+ *  Design Usage:
+ *  Gentle Wake Up turns on your lights slowly, allowing you to wake up more naturally. Optionally turn on more things,
+ *  or send yourself a push for a more gentle nudge into the waking world (you may want to set your normal alarm as a backup plan).
+ *
+ *  Copyright 2019 Bryan Turcotte (@bptworld)
+ *
+ *  This App is free.  If you like and use this app, please be sure to give a shout out on the Hubitat forums to let
+ *  people know that it exists!  Thanks.
+ *
+ *  Remember...I am not a programmer, everything I do takes a lot of time and research!
+ *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
+ *
+ *  Paypal at: https://paypal.me/bptworld
+ *
+ *-------------------------------------------------------------------------------------------------------------------
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  If modifying this project, please keep the above header intact and add your comments/credits below - Thank you! -  @BPTWorld
+ *
+ *  App and Driver updates can be found at https://github.com/bptworld/Hubitat
+ *
+ * ------------------------------------------------------------------------------------------------------------------------------
+ *
+ *  Changes:
+ *
+ *  V1.0.0 - 08/08/19 - Initial release.
+ *
+ */
+
+def setVersion() {
+	state.version = "v1.0.0"
+}
+
+definition(
+    name:"Gentle Wake Up",
+    namespace: "BPTWorld",
+    author: "Bryan Turcotte",
+    description: "Dim your lights up slowly, allowing you to wake up more naturally. Dim by level, color or temperature!",
+    category: "Convenience",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: "",
+	importUrl: ""
+)
+
+preferences {
+     page name: "mainPage", title: "", install: true, uninstall: true
+} 
+
+def installed() {
+    log.debug "Installed with settings: ${settings}"
+    initialize()
+}
+
+def updated() {
+    log.debug "Updated with settings: ${settings}"
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    log.info "There are ${childApps.size()} child apps"
+    childApps.each {child ->
+    log.info "Child app: ${child.label}"
+    }
+}
+
+def mainPage() {
+    dynamicPage(name: "mainPage") {
+    	installCheck()
+		if(state.appInstalled == 'COMPLETE'){
+			section(getFormat("title", "${app.label}")) {
+				paragraph "<div style='color:#1A77C9'>Gentle Wake Up turns on your lights slowly, allowing you to wake up more naturally. Optionally turn on more things, or send yourself a push for a more gentle nudge into the waking world (you may want to set your normal alarm as a backup plan).</div>"
+				paragraph getFormat("line")
+			}
+			section("Instructions:", hideable: true, hidden: true) {
+				paragraph "<b>Notes:</b>"
+				paragraph "Gentle Wake Up turns on your lights slowly, allowing you to wake up more naturally. Optionally turn on more things, or send yourself a push for a more gentle nudge into the waking world (you may want to set your normal alarm as a backup plan)."
+			}
+			section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
+				app(name: "anyOpenApp", appName: "Gentle Wake Up Child", namespace: "BPTWorld", title: "<b>Add a new 'Gentle Wake Up' child</b>", multiple: true)
+			}
+			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
+       			label title: "Enter a name for parent app (optional)", required: false
+ 			}
+			display2()
+		}
+	}
+}
+
+def installCheck(){         
+	state.appInstalled = app.getInstallationState() 
+	if(state.appInstalled != 'COMPLETE'){
+		section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
+  	}
+  	else{
+    	log.info "Parent Installed OK"
+  	}
+}
+
+def getImage(type) {
+    def loc = "<img src=https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/"
+    if(type == "Blank") return "${loc}blank.png height=40 width=5}>"
+}
+
+def getFormat(type, myText=""){
+	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
+    if(type == "line") return "\n<hr style='background-color:#1A77C9; height: 1px; border: 0;'></hr>"
+	if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
+}
+
+def display2(){
+	setVersion()
+	section() {
+		paragraph getFormat("line")
+		paragraph "<div style='color:#1A77C9;text-align:center'>Gentle Wake Up - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>Get app update notifications and more with <a href='https://github.com/bptworld/Hubitat/tree/master/Apps/App%20Watchdog' target='_blank'>App Watchdog</a><br>${state.version}</div>"
+	}       
+}         
