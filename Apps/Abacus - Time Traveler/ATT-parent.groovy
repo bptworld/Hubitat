188a4be90c5b826197ed853e4e98343eafe963bf
bptworld <bryan@bptworld.com>
2019-08-19 21:15:30 -0400

    Update ATT-parent.groovy

--- Apps/Abacus - Time Traveler/ATT-parent.groovy
+++ Apps/Abacus - Time Traveler/ATT-parent.groovy
@@ -37,6 +37,8 @@
  *
  *  Changes:
  *
+ *  V2.0.0 - 08/18/19 - Now App Watchdog 2 compliant
+ *  V1.0.5 - 03/21/19 - Broke up child apps in 4 sections. Motion Sensors, Contact Sensors, Switch Devices and Thermostat Devices.
  *  V1.0.4 - 02/16/19 - Big maintenance release. Reworked a lot of code as I continue to learn new things.
  *  V1.0.3 - 01/15/19 - Updated footer with update check and links
  *  V1.0.2 - 01/06/19 - Squashed a bug in the Weekly count reset. Also added in a way to delete a single line from the reports.
@@ -46,8 +48,20 @@
  *
  */
 
-def setVersion() {
-	state.version = "v1.0.4"
+def setVersion(){
+	if(logEnable) log.debug "In setVersion - App Watchdog Parent app code"
+    // Must match the exact name used in the json file. ie. AppWatchdogParentVersion, AppWatchdogChildVersion or AppWatchdogDriverVersion
+    state.appName = "AbacusTimeTravelerParentVersion"
+	state.version = "v2.0.0"
+    
+    try {
+        if(sendToAWSwitch && awDevice) {
+            awInfo = "${state.appName}:${state.version}"
+		    awDevice.sendAWinfoMap(awInfo)
+            if(logEnable) log.debug "In setVersion - Info was sent to App Watchdog"
+            schedule("0 0 3 ? * * *", setVersion)
+	    }
+    } catch (e) { log.error "In setVersion - ${e}" }
 }
 
 definition(
@@ -97,8 +111,30 @@ def mainPage() {
 				paragraph "Also, times are not added into totals until the device turns off (off/inactive/closed/idle)"
 			}
 			section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
-				app(name: "anyOpenApp", appName: "Abacus - Time Traveler Child", namespace: "BPTWorld", title: "<b>Add a new 'Abacus - Time Traveler' child</b>", multiple: true)
+				app(name: "anyOpenApp", appName: "Abacus - Time Traveler Motion Child", namespace: "BPTWorld", title: "<b>Add a new 'Abacus - Time Traveler Motion' child</b>", multiple: true)
+				app(name: "anyOpenApp", appName: "Abacus - Time Traveler Contact Child", namespace: "BPTWorld", title: "<b>Add a new 'Abacus - Time Traveler Contact' child</b>", multiple: true)
+				app(name: "anyOpenApp", appName: "Abacus - Time Traveler Switch Child", namespace: "BPTWorld", title: "<b>Add a new 'Abacus - Time Traveler Switch' child</b>", multiple: true)
+				app(name: "anyOpenApp", appName: "Abacus - Time Traveler Thermostat Child", namespace: "BPTWorld", title: "<b>Add a new 'Abacus - Time Traveler Thermostat' child</b>", multiple: true)
 			}
+            section(getFormat("header-green", "${getImage("Blank")}"+" App Watchdog")) {
+			    paragraph "This app supports Device Watchdog."
+                input(name: "sendToAWSwitch", type: "bool", defaultValue: "false", title: "Send this apps version data to App Watchdog?", description: "Update App Watchdog", submitOnChange: "true")
+                if(sendToAWSwitch) input(name: "awDevice", type: "capability.actuator", title: "Vitual Device created to send the AW Data to:", submitOnChange: true, required: true, multiple: false)
+			    if(sendToAWSwitch && awDevice) setVersion()
+            }
+            // ** App Watchdog Code **
+            section("This app supports App Watchdog 2! Click here for more Information", hideable: true, hidden: true) {
+				paragraph "<b>Information</b><br>See if any compatible app needs an update, all in one place!"
+                paragraph "<b>Requirements</b><br> - Must install the app 'App Watchdog'. Please visit <a href='https://community.hubitat.com/t/release-app-watchdog/9952' target='_blank'>this page</a> for more information.<br> - When you are ready to go, turn on the switch below<br> - Then select 'App Watchdog Data' from the dropdown.<br> - That's it, you will now be notified automaticaly of updates."
+                input(name: "sendToAWSwitch", type: "bool", defaultValue: "false", title: "Use App Watchdog to track this apps version info?", description: "Update App Watchdog", submitOnChange: "true")
+			}
+            if(sendToAWSwitch) {
+                section(getFormat("header-green", "${getImage("Blank")}"+" App Watchdog 2")) {    
+                    if(sendToAWSwitch) input(name: "awDevice", type: "capability.actuator", title: "Please select 'App Watchdog Data' from the dropdown", submitOnChange: true, required: true, multiple: false)
+			        if(sendToAWSwitch && awDevice) setVersion()
+                }
+            }
+            // ** End App Watchdog Code **
 			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
        			label title: "Enter a name for parent app (optional)", required: false
  			}
@@ -130,8 +166,8 @@ def getFormat(type, myText=""){
 }
 
 def display2(){
+    setVersion()
 	section() {
-		setVersion()
 		paragraph getFormat("line")
 		paragraph "<div style='color:#1A77C9;text-align:center'>Abacus - Time Traveler - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>Get app update notifications and more with <a href='https://github.com/bptworld/Hubitat/tree/master/Apps/App%20Watchdog' target='_blank'>App Watchdog</a><br>${state.version}</div>"
 	}       
