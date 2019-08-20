6f8a9708cbc972244cc2fc1f86d030702ca17188
bptworld <bryan@bptworld.com>
2019-08-19 21:36:56 -0400

    Update DS-child.groovy

--- Apps/Device Sequencer/DS-child.groovy
+++ Apps/Device Sequencer/DS-child.groovy
@@ -35,6 +35,7 @@
  *
  *  Changes:
  *
+ *  V2.0.0 - 08/18/19 - Now App Watchdog compliant
  *  V1.0.5 - 04/16/19 - Fixed some code 
  *  V1.0.4 - 04/15/19 - Code cleanup
  *  V1.0.3 - 03/12/19 - Fixed pause
@@ -44,8 +45,21 @@
  *
  */
 
-def setVersion() {
-	state.version = "v1.0.5"
+def setVersion(){
+    // *  V2.0.0 - 08/18/19 - Now App Watchdog compliant
+	if(logEnable) log.debug "In setVersion - App Watchdog Child app code"
+    // Must match the exact name used in the json file. ie. AppWatchdogParentVersion, AppWatchdogChildVersion or AppWatchdogDriverVersion
+    state.appName = "DeviceSequencerChildVersion"
+	state.version = "v2.0.0"
+    
+    try {
+        if(parent.sendToAWSwitch && parent.awDevice) {
+            awInfo = "${state.appName}:${state.version}"
+		    parent.awDevice.sendAWinfoMap(awInfo)
+            if(logEnable) log.debug "In setVersion - Info was sent to App Watchdog"
+            schedule("0 0 3 ? * * *", setVersion)
+	    }
+    } catch (e) { log.error "In setVersion - ${e}" }
 }
 
 definition(
