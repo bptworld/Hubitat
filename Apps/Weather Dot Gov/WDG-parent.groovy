5b126fd21613491e034588a09c9d37e1a39faeab
BPTWorld <bryan@bptworld.com>
2020-04-18 08:45:45 -0400

    Update WDG-parent.groovy

--- Apps/Weather Dot Gov/WDG-parent.groovy
+++ Apps/Weather Dot Gov/WDG-parent.groovy
@@ -37,8 +37,19 @@
  *
  */
 
-def setVersion() {
+def setVersion(){
+	if(logEnable) log.debug "In setVersion - App Watchdog Parent app code"
+    // Must match the exact name used in the json file. ie. AppWatchdogParentVersion
+    state.appName = "Weather Dot GovParentVersion"
 	state.version = "v1.0.0"
+    
+    try {
+        if(sendToAWSwitch && awDevice) {
+            awInfo = "${state.appName}:${state.version}"
+		    awDevice.sendAWinfoMap(awInfo)
+            if(logEnable) log.debug "In setVersion - Info was sent to App Watchdog"
+	    }
+    } catch (e) { log.error "In setVersion - ${e}" }
 }
 
 definition(
@@ -74,6 +85,7 @@ def initialize() {
     childApps.each {child ->
     	log.info "Child app: ${child.label}"
     }
+    if(awDevice) schedule("0 0 3 ? * * *", setVersion)
 }
 
 def mainPage() {
@@ -88,7 +100,19 @@ def mainPage() {
 			section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
 				app(name: "anyOpenApp", appName: "Weather Dot Gov Child", namespace: "BPTWorld", title: "<b>Add a new 'Weather Dot Gov' child</b>", multiple: true)
 			}
-            
+            // ** App Watchdog Code **
+            section("This app supports App Watchdog 2! Click here for more Information", hideable: true, hidden: true) {
+				paragraph "<b>Information</b><br>See if any compatible app needs an update, all in one place!"
+                paragraph "<b>Requirements</b><br> - Must install the app 'App Watchdog'. Please visit <a href='https://community.hubitat.com/t/release-app-watchdog/9952' target='_blank'>this page</a> for more information.<br> - When you are ready to go, turn on the switch below<br> - Then select 'App Watchdog Data' from the dropdown.<br> - That's it, you will now be notified automaticaly of updates."
+                input(name: "sendToAWSwitch", type: "bool", defaultValue: "false", title: "Use App Watchdog to track this apps version info?", description: "Update App Watchdog", submitOnChange: "true")
+			}
+            if(sendToAWSwitch) {
+                section(getFormat("header-green", "${getImage("Blank")}"+" App Watchdog 2")) {    
+                    if(sendToAWSwitch) input(name: "awDevice", type: "capability.actuator", title: "Please select 'App Watchdog 2 Data' from the dropdown", submitOnChange: true, required: true, multiple: false)
+			        if(sendToAWSwitch && awDevice) setVersion()
+                }
+            }
+            // ** End App Watchdog Code **
 			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
        			label title: "Enter a name for this parent app", required: false
  			}
@@ -135,6 +159,6 @@ def display2(){
     setVersion()
 	section() {
 		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>Weather Dot Gov - @BPTWorld<br>${state.version}</div>"
+		paragraph "<div style='color:#1A77C9;text-align:center'>Weather Dot Gov - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>${state.version}</div>"
 	}       
 }         
