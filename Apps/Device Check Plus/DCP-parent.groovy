547889b0cd6e1ccb3eaf272c683f73d5b9a4f0a9
BPTWorld <bryan@bptworld.com>
2020-04-27 19:13:53 -0400

    Update DCP-parent.groovy

--- Apps/Device Check Plus/DCP-parent.groovy
+++ Apps/Device Check Plus/DCP-parent.groovy
@@ -4,7 +4,7 @@
  *  Design Usage:
  *  Check selected devices, then warn you what's not in the right state.
  *
- *  Copyright 2019 Bryan Turcotte (@bptworld)
+ *  Copyright 2019-2020 Bryan Turcotte (@bptworld)
  *
  *  This App is free.  If you like and use this app, please be sure to mention it on the Hubitat forums!  Thanks.
  *
@@ -33,24 +33,14 @@
  *
  *  Changes:
  *
- *  V1.0.1 - 10/16/19 - Cosmetic changes
- *  V1.0.0 - 10/13/19 - Initial release
+ *  1.0.2 - 04/27/20 - Cosmetic changes
+ *  1.0.1 - 10/16/19 - Cosmetic changes
+ *  1.0.0 - 10/13/19 - Initial release
  *
  */
 
 def setVersion(){
-	if(logEnable) log.debug "In setVersion - App Watchdog Parent app code"
-    // Must match the exact name used in the json file. ie. AppWatchdogParentVersion
-    state.appName = "DeviceCheckPlusParentVersion"
-	state.version = "v1.0.1"
-    
-    try {
-        if(sendToAWSwitch && awDevice) {
-            awInfo = "${state.appName}:${state.version}"
-		    awDevice.sendAWinfoMap(awInfo)
-            if(logEnable) log.debug "In setVersion - Info was sent to App Watchdog"
-	    }
-    } catch (e) { log.error "In setVersion - ${e}" }
+	state.version = "1.0.2"
 }
 
 definition(
@@ -86,7 +76,6 @@ def initialize() {
     childApps.each {child ->
     	log.info "Child app: ${child.label}"
     }
-    if(awDevice) schedule("0 0 3 ? * * *", setVersion)
 }
 
 def mainPage() {
@@ -101,19 +90,7 @@ def mainPage() {
 			section(getFormat("header-green", "${getImage("Blank")}"+" Child Apps")) {
 				app(name: "anyOpenApp", appName: "Device Check Plus Child", namespace: "BPTWorld", title: "<b>Add a new 'Device Check Plus' child</b>", multiple: true)
 			}
-            // ** App Watchdog Code **
-            section("This app supports App Watchdog 2! Click here for more Information", hideable: true, hidden: true) {
-				paragraph "<b>Information</b><br>See if any compatible app needs an update, all in one place!"
-                paragraph "<b>Requirements</b><br> - Must install the app 'App Watchdog'. Please visit <a href='https://community.hubitat.com/t/release-app-watchdog/9952' target='_blank'>this page</a> for more information.<br> - When you are ready to go, turn on the switch below<br> - Then select 'App Watchdog Data' from the dropdown.<br> - That's it, you will now be notified automaticaly of updates."
-                input(name: "sendToAWSwitch", type: "bool", defaultValue: "false", title: "Use App Watchdog to track this apps version info?", description: "Update App Watchdog", submitOnChange: "true")
-			}
-            if(sendToAWSwitch) {
-                section(getFormat("header-green", "${getImage("Blank")}"+" App Watchdog 2")) {    
-                    if(sendToAWSwitch) input(name: "awDevice", type: "capability.actuator", title: "Please select 'App Watchdog 2 Data' from the dropdown", submitOnChange: true, required: true, multiple: false)
-			        if(sendToAWSwitch && awDevice) setVersion()
-                }
-            }
-            // ** End App Watchdog Code **
+            
 			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
        			label title: "Enter a name for this parent app", required: false
  			}
@@ -160,6 +137,6 @@ def display2(){
     setVersion()
 	section() {
 		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>Device Check Plus - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br>Get app update notifications and more with <a href='https://github.com/bptworld/Hubitat/tree/master/Apps/App%20Watchdog' target='_blank'>App Watchdog</a><br>${state.version}</div>"
+		paragraph "<div style='color:#1A77C9;text-align:center'>Device Check Plus - @BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br><a href='https://paypal.me/bptworld' target='_blank'><img src='https://raw.githubusercontent.com/bptworld/Hubitat/master/resources/images/pp.png'></a><br>${state.version}</div>"
 	}       
 }         
