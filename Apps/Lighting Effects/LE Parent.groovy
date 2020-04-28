542a363bdd787e1a77aea5cf89ce90b8ee691dfb
BPTWorld <bryan@bptworld.com>
2020-04-28 15:05:29 -0400

    Update LE Parent.groovy

--- Apps/Lighting Effects/LE Parent.groovy
+++ Apps/Lighting Effects/LE Parent.groovy
@@ -38,11 +38,13 @@
  *
  *  Changes:
  *
- * V2.0.1 - 04/12/20 - Removed color options from parent app
+ *  2.0.2 - 04/27/20 - Cosmetic changes
+ *  2.0.1 - 04/12/20 - Removed color options from parent app
  */
 
 def setVersion(){
-	state.version = "v2.0.1"
+    state.name = "Lighting Effects"
+	state.version = "2.0.2"
 }
 
 definition(
@@ -82,7 +84,6 @@ def mainPage() {
     dynamicPage(name: "mainPage") {
     	installCheck()
 		if(state.appInstalled == 'COMPLETE'){
-            display()
             section("Instructions:", hideable: true, hidden: true) {
                 paragraph "<b>Lighting Effects is designed to work with Hue devices. While other brands may work, nothing is guaranteed.</b> If not using Hue devices, you can try turning on 'Enable Hue in degrees (0-360)' for each device used with this app."
                 paragraph "<b>Fast Dimming:</b>"
@@ -111,7 +112,8 @@ def mainPage() {
   	}
 }
 
-def installCheck(){         
+def installCheck(){ 
+    display()
 	state.appInstalled = app.getInstallationState() 
 	if(state.appInstalled != 'COMPLETE'){
 		section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
@@ -127,27 +129,55 @@ def getImage(type) {					// Modified from @Stephack Code
     if(type == "checkMarkGreen") return "${loc}checkMarkGreen2.png height=30 width=30>"
     if(type == "optionsGreen") return "${loc}options-green.png height=30 width=30>"
     if(type == "optionsRed") return "${loc}options-red.png height=30 width=30>"
-    if(type == "cogWithWrench") return "${loc}cogWithWrench.png height=30 width=30>"
+    if(type == "instructions") return "${loc}instructions.png height=30 width=30>"
     if(type == "logo") return "${loc}logo.png height=60>"
 }
 
-def getFormat(type, myText=""){			// Modified from @Stephack Code   
+def getFormat(type, myText="") {			// Modified from @Stephack Code   
 	if(type == "header-green") return "<div style='color:#ffffff;font-weight: bold;background-color:#81BC00;border: 1px solid;box-shadow: 2px 3px #A9A9A9'>${myText}</div>"
     if(type == "line") return "<hr style='background-color:#1A77C9; height: 1px; border: 0;'>"
     if(type == "title") return "<h2 style='color:#1A77C9;font-weight: bold'>${myText}</h2>"
 }
 
 def display() {
-    section (getFormat("title", "${getImage("logo")}" + " Lighting Effects")) {
-        paragraph "<div style='color:#1A77C9'>Create a spooky, sparkly or party effect.</div>"
+    setVersion()
+    getHeaderAndFooter()
+    theName = app.label
+    if(theName == null || theName == "") theName = "New Child App"
+    section (getFormat("title", "${getImage("logo")}" + " ${state.name} - ${theName}")) {
+        paragraph "${state.headerMessage}"
 		paragraph getFormat("line")
 	}
 }
 
-def display2(){
-    setVersion()
+def display2() {
 	section() {
 		paragraph getFormat("line")
-		paragraph "<div style='color:#1A77C9;text-align:center'>Lighting Effects - @BPTWorld<br>${state.version}</div>"
+		paragraph "<div style='color:#1A77C9;text-align:center;font-size:20px;font-weight:bold'>${state.name} - ${state.version}</div>"
+        paragraph "${state.footerMessage}"
 	}       
-} 
+}
+
+def getHeaderAndFooter() {
+    if(logEnable) log.debug "In getHeaderAndFooter (${state.version})"
+    def params = [
+	    uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/info.json",
+		requestContentType: "application/json",
+		contentType: "application/json",
+		timeout: 30
+	]
+    
+    try {
+        def result = null
+        httpGet(params) { resp ->
+            state.headerMessage = resp.data.headerMessage
+            state.footerMessage = resp.data.footerMessage
+        }
+        if(logEnable) log.debug "In getHeaderAndFooter - headerMessage: ${state.headerMessage}"
+        if(logEnable) log.debug "In getHeaderAndFooter - footerMessage: ${state.footerMessage}"
+    }
+    catch (e) {
+        state.headerMessage = "<div style='color:#1A77C9'><a href='https://github.com/bptworld/Hubitat' target='_blank'>BPTWorld Apps and Drivers</a></div>"
+        state.footerMessage = "<div style='color:#1A77C9;text-align:center'>BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br><a href='https://paypal.me/bptworld' target='_blank'>Paypal</a></div>"
+    }
+}
