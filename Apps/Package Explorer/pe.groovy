fe92958e9132b36911b34318c7ff1f4afd96b4b0
BPTWorld <bryan@bptworld.com>
2020-05-07 13:06:36 -0400

    Update pe.groovy

--- Apps/Package Explorer/pe.groovy
+++ Apps/Package Explorer/pe.groovy
@@ -40,6 +40,7 @@
  *
  *  Changes:
  *
+ *  1.0.2 - 05/07/20 - Added 'Developer Options' Search, cosmetic changes
  *  1.0.1 - 05/02/20 - Minor changes
  *  1.0.0 - 05/01/20 - Initial release.
  *
@@ -53,7 +54,7 @@ import groovy.transform.Field
 
 def setVersion(){
     state.name = "Package Explorer"
-	state.version = "1.0.1"
+	state.version = "1.0.2"
 }
 
 definition(
@@ -65,13 +66,14 @@ definition(
     iconUrl: "",
     iconX2Url: "",
     iconX3Url: "",
-	importUrl: "",
+	importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Package%20Explorer/pe.groovy",
 )
 
 preferences {
     page(name: "pageConfig")
     page(name: "searchOptions", title: "", install: false, uninstall: true, nextPage: "pageConfig")
     page(name: "categoryOptions", title: "", install: false, uninstall: true, nextPage: "pageConfig")
+    page(name: "developerOptions", title: "", install: false, uninstall: true, nextPage: "pageConfig")
 }
 
 def pageConfig() {
@@ -81,23 +83,28 @@ def pageConfig() {
     
     dynamicPage(name: "", title: "", install: true, uninstall: true) {
         installCheck()
-		if(state.appInstalled == 'COMPLETE'){          
+		if(state.appInstalled == 'COMPLETE'){    
+            performRepositoryRefresh()
             section("${getImage('instructions')} <b>Instructions:</b>", hideable: true, hidden: true) {
                 paragraph "<b>Notes:</b>"
                 paragraph "Conveniently explore the apps and drivers available within the Hubitat Package Manager.<br>All credit goes to @dman2306 for his amazing work on HPM."
             }
-
-            section(getFormat("header-green", "${getImage("Blank")}"+" Select Options")) {
-                app.removeSetting("pkgCategory")  // reset pkgCategory
-                app.removeSetting("pkgSearch")  // reset pkgSearch
-                app.removeSetting("pkgSearchNOT")  // reset pkgSearchNOT
-                app.removeSetting("pkgSearchType")  // reset pkgSearchType
+            section(getFormat("header-green", "${getImage("Blank")}"+" HPM Stats")) {
+                paragraph "Hubitat Package Manager is now serving <b>${state.packCount} packages</b> (apps/drivers) from <b>${state.repoCount} developers</b>!"
+            }
+            
+            section(getFormat("header-green", "${getImage("Blank")}"+" Search Options")) {
+                app.removeSetting("pkgCategory")       // reset pkgCategory
+                app.removeSetting("pkgSearch")         // reset pkgSearch
+                app.removeSetting("pkgSearchNOT")      // reset pkgSearchNOT
+                app.removeSetting("pkgSearchType")     // reset pkgSearchType
                 
-                input "installedRepositories", "enum", title: "Available Repositories", options:state.reposToShow, multiple:true, required:true, submitOnChange:true
-                if(installedRepositories) performRepositoryRefresh()
+                //input "installedRepositories", "enum", title: "Available Repositories", options:state.reposToShow, multiple:true, required:true, submitOnChange:true     
+                app.updateSetting("installedRepositories", state.reposToShow)
 
-                href "categoryOptions", title:"Category Options", description:"Use Categories to search for an app or driver"
-                href "searchOptions", title:"Keyword Options", description:"Use Keywords to search for an app or driver"
+                href "categoryOptions", title:"Category Options", description:"See all Apps and Drivers the selected Category has to offer"
+                href "developerOptions", title:"Developer Options", description:"See all Apps and Drivers the selected Developer has to offer"
+                href "searchOptions", title:"Keyword Options", description:"Use Keywords to search for an App or Driver across all Developers and Categories"
             }
 
             section(getFormat("header-green", "${getImage("Blank")}"+" Maintenance")) {
@@ -124,7 +131,7 @@ def categoryOptions() {
     dynamicPage(name: "categoryOptions", title: "", install: false, uninstall:false){
         display()
 		section(getFormat("header-green", "${getImage("Blank")}"+" Search Options")) {
-            input "pkgCategory", "enum", title: "Choose a Category To Explore", options:state.categories, required:false, submitOnChange:true, width:6
+            input "pkgCategory", "enum", title: "Choose a Category To Explore", options:state.categories, required:false, submitOnChange:true
             if(pkgCategory) {
                 input "sortBy", "bool", title: "Sort packages by Name (off) or by Author (on)", description: "Sorting", defaultValue:false, submitOnChange:true
             }
@@ -132,6 +139,25 @@ def categoryOptions() {
         
         if(pkgCategory) {
             findPackagesByCategory()
+            section(getFormat("header-green", "${getImage("Blank")}"+" ${state.resultsTitle}")) {
+                paragraph appsList
+            }
+	    }
+    }
+}
+
+def developerOptions() {
+    dynamicPage(name: "developerOptions", title: "", install: false, uninstall:false){
+        display()
+		section(getFormat("header-green", "${getImage("Blank")}"+" Search Options")) {
+            input "pkgAuthor", "enum", title: "Choose a Developer To Explore", options:state.allDevNames, required:true, submitOnChange:true
+            if(pkgAuthor) {
+                input "sortBy", "bool", title: "Sort packages by App Name (off) or by Category (on)", description: "Sorting", defaultValue:false, submitOnChange:true
+            }
+        }
+        
+        if(pkgAuthor) {
+            findPackagesByDeveloper()
             section(getFormat("header-green", "${getImage("Blank")}"+" ${state.resultsTitle}")) {
                 paragraph appsList
             }
@@ -163,7 +189,8 @@ def searchOptions() {
 def findPackagesByCategory() {
     if(logEnable) log.debug "In findPackagesByCategory"   
     if(!sortBy) allPackages = state.allPackages.sort{ name-> name.name}
-    if(sortBy) allPackages = state.allPackages.sort{ name-> name.author}
+    if(sortBy) allPackages = state.allPackages.sort{ name-> name.author}    
+    state.packageCount = allPackages.size()
                 
     state.resultsTitle = "<b>Search Results for Category - ${pkgCategory}</b>"
     appsList = ""
@@ -194,11 +221,47 @@ def findPackagesByCategory() {
         }
     }
 }
-             
+    
+def findPackagesByDeveloper() {
+    if(logEnable) log.debug "In findPackagesByDeveloper"
+    if(!sortBy) allPackages = state.allPackages.sort{ name-> name.name}
+    if(sortBy) allPackages = state.allPackages.sort{ name-> name.category}
+    
+    //allPackages = state.allPackages.sort{ name-> name.author}    
+    state.packageCount = allPackages.size()
+            
+    state.resultsTitle = "<b>Search Results for Developer - ${pkgAuthor}</b>"
+    appsList = ""
+    
+	for (pkg in allPackages) {
+        if(logEnable) log.debug "In findPackagesByDeveloper - name: ${pkg.author} - search: ${pkgAuthor}"
+		if (pkg.author == pkgAuthor) {      
+            def info = getJSONFile(pkg.location)
+                        
+            theLinks = ""            
+            if(info.documentationLink) {
+                theLinks += "| <a href='${info.documentationLink}' target='_blank'>Documentation</a> "
+            }
+            if(info.communityLink) {
+                theLinks += "| <a href='${info.communityLink}' target='_blank'>Community Thread</a> "
+            }
+            if(pkg.gitHubUrl) {
+                theLinks += "| <a href='${pkg.gitHubUrl}' target='_blank'>GitHub</a> "
+            }
+            if(pkg.payPalUrl) {
+                theLinks += "| <a href='${pkg.payPalUrl}' target='_blank'>Donate</a> "
+            }
+            if(theLinks != "") theLinks += "|"               
+                
+            appsList += "<div style='background-color: white;width: 90%;border: 1px solid grey;border-radius: 5px;box-shadow: 3px 3px;padding: 20px;margin: 20px;'>(${pkg.category}) - <b>${pkg.name}</b> - (${pkg.author})<br>${theLinks}<br>${pkg.description}</div>"         
+        }
+    }
+}
+
 def findPackagesBySearch() {
     if(logEnable) log.debug "In findPackagesBySearch"
-    if(!sortBy) allPackages = state.allPackages.sort{ name-> name.name}
-    if(sortBy) allPackages = state.allPackages.sort{ name-> name.author}
+    if(!sortBy) allPackages = state.allPackages.sort{ name-> name.name }
+    if(sortBy) allPackages = state.allPackages.sort{ name-> name.author }
                 
     state.resultsTitle = "<b>Search Results for Keyword - ${pkgSearch}</b>"
     theLinks = "" 
@@ -246,20 +309,25 @@ def findPackagesBySearch() {
 def reposToShowHandler() {        // Code by dman2306
     if(logEnable) log.debug "In reposToShowHandler"
     def reposToShow = [:]
+    state.repoCount = 0
 	listOfRepositories.repositories.each { r -> reposToShow << ["${r.location}":r.name] }
 	if (state.customRepositories != null)
 		state.customRepositories.each { r -> reposToShow << ["${r.key}":r.value] }
 	state.reposToShow = reposToShow.sort { r -> r.value }
+    state.repoCount = state.reposToShow.size()
 }
 
 def getRepoName(location) {        // Code by dman2306
 	return listOfRepositories.repositories.find { it -> it.location == location }?.name
 }
 
-def performRepositoryRefresh() {        // Code by dman2306
+def performRepositoryRefresh() {        // Code by dman2306, mods by bptworld
     if(logEnable) log.debug "In performRepositoryRefresh"
 	allPackages = []
 	categories = []
+    allDevNames = []
+    tempList = ""
+    state.packCount = 0
 
 	for (repo in installedRepositories) {
 		def repoName = getRepoName(repo)
@@ -280,16 +348,22 @@ def performRepositoryRefresh() {        // Code by dman2306
 				category: pkg.category
 			]
 			allPackages << pkgDetails
+            if(!tempList.contains("${pkgDetails.author}")) {
+                tempList += "${pkgDetails.author}, "
+                allDevNames << pkgDetails.author
+            }
 			if (!categories.contains(pkgDetails.category))
 				categories << pkgDetails.category
 		}
 	}
-	state.allPackages = allPackages.sort()
+    state.allPackages = allPackages.sort()
 	state.categories = categories.sort()
+    state.allDevNames = allDevNames.sort()    
+    state.packCount = allPackages.size()
 }
 
 def getJSONFile(uri) {        // Code by dman2306
-    if(logEnable) log.debug "In getJSONFile"
+    //if(logEnable) log.debug "In getJSONFile"
 	try {
 		def fileContents = downloadFile(uri)
 		return new groovy.json.JsonSlurper().parseText(fileContents)
@@ -314,7 +388,7 @@ def initialize() {
 }
 
 def downloadFile(file) {        // Code by dman2306
-    if(logEnable) log.debug "In downloadFile"
+    //if(logEnable) log.debug "In downloadFile"
 	try {
 		def params = [
 			uri: file,
@@ -379,8 +453,6 @@ def getAppList() {    // Thanks to gavincampbell for the code below!
     } catch (e) {
 		log.error "Error retrieving installed apps: ${e}"
     }
-    //log.warn "result: ${state.result}"
-	//return result
 }
 
 // ********** Normal Stuff **********
@@ -437,8 +509,8 @@ def getHeaderAndFooter() {
             state.headerMessage = resp.data.headerMessage
             state.footerMessage = resp.data.footerMessage
         }
-        if(logEnable) log.debug "In getHeaderAndFooter - headerMessage: ${state.headerMessage}"
-        if(logEnable) log.debug "In getHeaderAndFooter - footerMessage: ${state.footerMessage}"
+        //if(logEnable) log.debug "In getHeaderAndFooter - headerMessage: ${state.headerMessage}"
+        //if(logEnable) log.debug "In getHeaderAndFooter - footerMessage: ${state.footerMessage}"
     }
     catch (e) {
         state.headerMessage = "<div style='color:#1A77C9'><a href='https://github.com/bptworld/Hubitat' target='_blank'>BPTWorld Apps and Drivers</a></div>"
