9425603a269095c25910fd24747aaff701f65069
BPTWorld <bryan@bptworld.com>
2020-07-30 08:45:46 -0400

    Update WMU-child.groovy

--- Apps/Wake Me Up/WMU-child.groovy
+++ Apps/Wake Me Up/WMU-child.groovy
@@ -55,6 +55,7 @@ definition(
     iconUrl: "",
     iconX2Url: "",
     iconX3Url: "",
+    importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Wake%20Me%20Up/WMU-child.groovy",
 )
 
 preferences {
