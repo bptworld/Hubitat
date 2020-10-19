02f4584fa05e76c7cd9f92c799c96cedbab8fbdc
BPTWorld <bryan@bptworld.com>
2020-10-18 21:23:49 -0400

    Update EE-parent.groovy

--- Apps/Event Engine/EE-parent.groovy
+++ Apps/Event Engine/EE-parent.groovy
@@ -33,15 +33,19 @@
  *
  *  Changes:
  *
+ *  1.0.3 - 10/18/20 - Added Global Variables
  *  1.0.2 - 09/13/20 - Bring on the Cogs!
  *  1.0.1 - 09/12/20 - Name change
  *  1.0.0 - 09/05/20 - Initial release.
  *
  */
 
+import groovy.time.TimeCategory
+import java.text.SimpleDateFormat
+
 def setVersion(){
     state.name = "Event Engine"
-	state.version = "1.0.2"
+	state.version = "1.0.3"
 }
 
 definition(
@@ -66,7 +70,13 @@ def installed() {
 
 def updated() {
     log.debug "Updated with settings: ${settings}"
+    unschedule()
     unsubscribe()
+    if(logEnable && logOffTime == "1 Hour") runIn(3600, logsOff, [overwrite:false])
+    if(logEnable && logOffTime == "2 Hours") runIn(7200, logsOff, [overwrite:false])
+    if(logEnable && logOffTime == "3 Hours") runIn(10800, logsOff, [overwrite:false])
+    if(logEnable && logOffTime == "4 Hours") runIn(14400, logsOff, [overwrite:false])
+    if(logEnable && logOffTime == "5 Hours") runIn(18000, logsOff, [overwrite:false])
     initialize()
 }
 
@@ -85,16 +95,78 @@ def mainPage() {
 				paragraph "<b>Information</b>"
 				paragraph "Automate your world with easy to use Cogs. Rev up complex automations with just a few clicks!"
 			}
+            
 			section(getFormat("header-green", "${getImage("Blank")}"+" Cogs")) {
 				app(name: "anyOpenApp", appName: "Event Engine Cog", namespace: "BPTWorld", title: "<b>Add a new Cog to Event Engine</b>", multiple: true)
 			}
             
+            section(getFormat("header-green", "${getImage("Blank")}"+" Global Variables")) {
+                paragraph "Global Variables can be accessed and updated by any EE Cog. A great way to share variables between Cogs!"
+                paragraph "- <b>To add or edit a variable</b>, simply fill in the Name and Value below. Variable will be added as soon as you click outside the value field.<br>- <b>To delete a variable</b>, enter in the Name and flip the Add/Edit Delete switch to on."
+				input "gName", "text", title: "Variable Name", required:false, width:6
+                input "gValue", "text", title: "Set initial value", required:false, submitOnChange:true, width:6
+                input "gvAED", "bool", title: "Add/Edit (off) or Delete (on)", description: "Add Edit Delete", defaultValue:false, submitOnChange:true, width:12
+                if(gName && gvAED) {
+                    gVariablesHandler("del;nothing")
+                } else if(gName && gValue) {
+                    gVariablesHandler("add;nothing")
+                } else {
+                    gVariablesHandler("refresh;nothing")
+                }
+                paragraph "<hr>"
+                paragraph "${state.niceMap}"
+                paragraph "<hr>"
+			}
+            
 			section(getFormat("header-green", "${getImage("Blank")}"+" General")) {
        			label title: "Enter a name for parent app (optional)", required: false
+                input "logEnable", "bool", title: "Enable Debug Options", description: "Log Options", defaultValue:false, submitOnChange:true
+                if(logEnable) {
+                    input "logOffTime", "enum", title: "Logs Off Time", required:false, multiple:false, options: ["1 Hour", "2 Hours", "3 Hours", "4 Hours", "5 Hours"]
+                }
  			}
 			display2()
 		}
 	}
+}
+
+def gVariablesHandler(data) {
+    if(logEnable) log.debug "In gVariablesHandler (${state.version}) - data: ${data}"
+    def (theType, newData) = data.split(";")
+    if(state.gvMap == null) state.gvMap = [:]
+    
+    if(theType == "fromChild") {
+        def (cName, cValue) = newData.split(":")
+        state.gvMap.put(cName,cValue)
+    } else if(theType == "add") {
+        state.gvMap.put(gName,gValue)
+    } else if(theType == "del") {
+        state.gvMap.remove(gName)
+    }
+      
+    if(state.gvMap) {
+        niceMap =  "<table width=50% align=center><tr><td><b><u>Name</u></b><td>   <td><b><u>Value</u></b>"
+        def theData = "${state.gvMap}".split(",")
+        theData.each { it -> 
+            def (name, value) = it.split(":")
+            if(name.startsWith(" ") || name.startsWith("[")) name = name.substring(1)
+            value = value.replace("]","")
+            niceMap += "<tr><td>${name}<td> - <td>${value}"
+        }                
+        niceMap += "</table>"
+    }
+    state.niceMap = niceMap
+    
+    app.removeSetting("gName")
+    app.removeSetting("gValue")
+    app?.updateSetting("gvAED",[value:"false",type:"bool"])
+    sendToChildren()
+}
+
+def sendToChildren() {
+    if(state.gvMap) childApps.each { child ->
+        child.globalVariablesHandler(state.gvMap)
+    }
 }
 
 def installCheck(){
@@ -104,8 +176,13 @@ def installCheck(){
 		section{paragraph "Please hit 'Done' to install '${app.label}' parent app "}
   	}
   	else{
-    	log.info "Parent Installed OK"
+    	if(logEnable) log.info "Parent Installed OK"
   	}
+}
+
+def logsOff() {
+    log.info "${app.label} - Debug logging auto disabled"
+    app?.updateSetting("logEnable",[value:"false",type:"bool"])
 }
 
 def getImage(type) {					// Modified from @Stephack Code
@@ -144,25 +221,40 @@ def display2() {
 }
 
 def getHeaderAndFooter() {
-    //if(logEnable) log.debug "In getHeaderAndFooter (${state.version})"
-    def params = [
-	    uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/info.json",
-		requestContentType: "application/json",
-		contentType: "application/json",
-		timeout: 30
-	]
-    
-    try {
-        def result = null
-        httpGet(params) { resp ->
-            state.headerMessage = resp.data.headerMessage
-            state.footerMessage = resp.data.footerMessage
+    timeSinceNewHeaders()
+    if(state.totalHours > 4) {
+        def params = [
+            uri: "https://raw.githubusercontent.com/bptworld/Hubitat/master/info.json",
+            requestContentType: "application/json",
+            contentType: "application/json",
+            timeout: 30
+        ]
+        try {
+            def result = null
+            httpGet(params) { resp ->
+                state.headerMessage = resp.data.headerMessage
+                state.footerMessage = resp.data.footerMessage
+            }
         }
-        //if(logEnable) log.debug "In getHeaderAndFooter - headerMessage: ${state.headerMessage}"
-        //if(logEnable) log.debug "In getHeaderAndFooter - footerMessage: ${state.footerMessage}"
+        catch (e) { }
     }
-    catch (e) {
-        state.headerMessage = "<div style='color:#1A77C9'><a href='https://github.com/bptworld/Hubitat' target='_blank'>BPTWorld Apps and Drivers</a></div>"
-        state.footerMessage = "<div style='color:#1A77C9;text-align:center'>BPTWorld<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Find more apps on my Github, just click here!</a><br><a href='https://paypal.me/bptworld' target='_blank'>Paypal</a></div>"
+    if(state.headerMessage == null) state.headerMessage = "<div style='color:#1A77C9'><a href='https://github.com/bptworld/Hubitat' target='_blank'>BPTWorld Apps and Drivers</a></div>"
+    if(state.footerMessage == null) state.footerMessage = "<div style='color:#1A77C9;text-align:center'>BPTWorld Apps and Drivers<br><a href='https://github.com/bptworld/Hubitat' target='_blank'>Donations are never necessary but always appreciated!</a><br><a href='https://paypal.me/bptworld' target='_blank'><b>Paypal</b></a></div>"
+}
+
+def timeSinceNewHeaders() { 
+    if(state.previous == null) { 
+        prev = new Date()
+    } else {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+        prev = dateFormat.parse("${state.previous}".replace("+00:00","+0000"))
     }
+    def now = new Date()
+    use(TimeCategory) {
+        state.dur = now - prev
+        state.days = state.dur.days
+        state.hours = state.dur.hours
+        state.totalHours = (state.days * 24) + state.hours
+    }
+    state.previous = now
 }
