4c1d4ca9896128b4bc632f1915aba7773cbcaf5b
BPTWorld <bryan@bptworld.com>
2021-01-24 10:11:31 -0500

    Update SIP2IR Driver.groovy

--- Apps/Send IP2IR/SIP2IR Driver.groovy
+++ Apps/Send IP2IR/SIP2IR Driver.groovy
@@ -6,7 +6,7 @@
  *
  *  IR Codes can be found using Global Cache Control Tower IR Database, https://irdb.globalcache.com/
  *
- *  Copyright 2018-2019 Bryan Turcotte (@bptworld)
+ *  Copyright 2018-2021 Bryan Turcotte (@bptworld)
  *
  *  Thanks to Carson Dallum's (@cdallum) for the original IP2IR driver code that I based my driver off of.
  *  
@@ -42,76 +42,53 @@
  *
  *  Changes:
  *
- *  v1.0.5 - 01/03/20 - Fix for App Watchdog 2
- *  V1.0.4 - 08/29/19 - App Watchdog Compatible
- *  V1.0.3 - 04/16/19 - Code cleanup
- *  V1.0.2 - 12/06/18 - Minor changes and additonal error message. If the IP2IR unit is unplugged or loses connection for any
+ *  1.0.7 - 01/24/21 - Cosmetic changes
+ *  1.0.6 - 03/05/20 - Minor changes
+ *  1.0.5 - 01/03/20 - Fix for App Watchdog 2
+ *  1.0.4 - 08/29/19 - App Watchdog Compatible
+ *  1.0.3 - 04/16/19 - Code cleanup
+ *  1.0.2 - 12/06/18 - Minor changes and additonal error message. If the IP2IR unit is unplugged or loses connection for any
  *			 			reason, simply go into the IP2IR Telnet device and press the 'Initialize' button.
- *  V1.0.1 - 11/01/18 - Merged pull request from DTTerastar resend the command if busy is received. 
- *  V1.0.0 - 10/15/18 - Initial release
+ *  1.0.1 - 11/01/18 - Merged pull request from DTTerastar resend the command if busy is received. 
+ *  1.0.0 - 10/15/18 - Initial release
  */
-
-def setVersion(){
-    appName = "IP2IRTelnet"
-	version = "v1.0.5" 
-    dwInfo = "${appName}:${version}"
-    sendEvent(name: "dwDriverInfo", value: dwInfo, displayed: true)
-}
-
-def updateVersion() {
-    log.info "In updateVersion"
-    setVersion()
-}
 
 metadata {
 	definition (name: "IP2IR Telnet", namespace: "BPTWorld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Send%20IP2IR/SIP2IR%20Driver.groovy") {
-    capability "Actuator"
-	capability "Initialize"
-    capability "Telnet"
-    capability "Notification"
-    capability "Speech Synthesis"
+        capability "Actuator"
+	    capability "Initialize"
+        capability "Telnet"
+        capability "Notification"
 
-    attribute "Telnet", ""
-        
-    attribute "dwDriverInfo", "string"
-    command "updateVersion"
+        attribute "telnet", ""
+    }
 }
     
 preferences() {    	
-        section(""){
-            input "ipaddress", "text", required: true, title: "iTach IP2IR IP Address", defaultValue: "0.0.0.0"
-            input "logEnable", "bool", title: "Enable logging", required: true, defaultValue: true
-        }
+    section() {
+        input "ipaddress", "text", required: true, title: "iTach IP2IR IP Address", defaultValue: "0.0.0.0"
+        input "logEnable", "bool", title: "Enable logging", required: true, defaultValue: true
     }
 }
 
-def speak(msg) {
+def deviceNotification(msg) {
     state.lastmsg = msg
-    if(logEnable) log.debug "In speak - Sending Message: ${msg}"
+    if(logEnable) log.debug "In deviceNotification - Sending Message: ${msg}"
     return new hubitat.device.HubAction("${msg}\n", hubitat.device.Protocol.TELNET)
 }
 
-def deviceNotification(message) {
-    if(logEnable) log.debug "In deviceNotification - Sending Message: ${message}"
-    speak(message)
-}
-
-def resend(){
+def resend() {
     if(logEnable) log.debug "RESEND!"
-    speak(state.lastmsg)
+    deviceNotification(state.lastmsg)
 }
 
-def initialize(){
-	try {
-		if(logEnable) log.debug "Opening telnet connection"
-		sendEvent([name: "telnet", value: "Opening"])
-        //telnetConnect([terminalType: 'VT100', termChars:[13]], "${ipaddress}", 4998, null, null)
-		telnetConnect([terminalType: 'VT100'], "${ipaddress}", 4998, null, null)
-		//give it a chance to start
-		pauseExecution(1000)
-		if(logEnable) log.debug "Telnet connection established"
-    } catch(e) {
-		log.warn "Initialize Error: ${e.message}"
+def isConnected() {
+    pauseExecution(1000)
+    def telnet1 = device.currentValue('telnet')
+    if(logEnable) log.debug "In isConnected: telnet status: ${telnet1}"
+    if(telnet1 != "Error") {
+        log.debug "Send IP2IR - Telnet connection established"
+        sendEvent(name: "telnet", value: "Connected")
     }
 }
 
@@ -123,10 +100,27 @@ def updated() {
 	initialize()
 }
 
+def initialize() {
+    state.aSetupInstructions = "<b>To setup this Device:</b><br> - Enter in your iTach IP Address and press 'Save Preferences'"
+    state.aTestingTheConnection = "<b>To test the telnet connection:</b><br> - Under 'Current States', it should say 'Connected'.<br> - If not press the 'Initialize' button. Make sure it now says connected.<br> - If not check your IP address."
+    state.aTestingTheCommands = "<b>To test a command:</b><br> - Have Logs open in another tab.<br> - Paste a valid 'send' command into the 'Device Notification' box and press the 'Device Notification' button.<br> - IR device should have responded (turned on/off, ect.).<br> - If device didn't respond, check logs for errors."
+	try {
+		if(logEnable) log.debug "Opening telnet connection"
+		sendEvent(name: "telnet", value: "Opening")
+        //telnetConnect([terminalType: 'VT100', termChars:[13]], "${ipaddress}", 4998, null, null)
+		telnetConnect([terminalType: 'VT100'], "${ipaddress}", 4998, null, null)
+		//give it a chance to start
+		isConnected()
+    } catch(e) {
+		log.warn "Initialize Error: ${e.message}"
+        sendEvent(name: "telnet", value: "Error")
+    }
+}
+
 def parse(String msg) {
     if(logEnable) log.debug "parse ${msg}"
-	sendEvent([name: "telnet", value: "Connected"])
-    if (msg == "busyIR,1:1,1"){
+	sendEvent(name: "telnet", value: "Connected")
+    if (msg == "busyIR,1:1,1") {
         runIn(1, resend)
     }
 }
@@ -135,8 +129,10 @@ def telnetStatus(String status) {
 	if(logEnable) log.debug "telnetStatus: ${status}"
 	if (status == "receive error: Stream is closed" || status == "send error: Broken pipe (Write failed)") {
 		log.error("Telnet connection dropped...PLEASE OPEN THE IP2IR TELNET DEVICE IN HE AND PRESS THE 'INITIALIZE' BUTTON")
-        sendEvent([name: "telnet", value: "Disconnected"])
+        sendEvent(name: "telnet", value: "Disconnected")
 		telnetClose()
 		runIn(60, initialize)
+    } else {
+        //sendEvent(name: "telnet", value: "${status}")
     }
 }
