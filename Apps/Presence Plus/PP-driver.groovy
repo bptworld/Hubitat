f649f0470f8c8c7d2b5e03a76bab0e12a5a1dc8a
BPTWorld <bryan@bptworld.com>
2021-06-24 13:15:52 -0400

    Update PP-driver.groovy

--- Apps/Presence Plus/PP-driver.groovy
+++ Apps/Presence Plus/PP-driver.groovy
@@ -4,11 +4,11 @@
  *  Design Usage:
  *  This driver works with the Presence Plus app.
  *
- *  Copyright 2019-2020 Bryan Turcotte (@bptworld)
+ *  Copyright 2019-2021 Bryan Turcotte (@bptworld)
  *  
  *  This App is free.  If you like and use this app, please be sure to mention it on the Hubitat forums!  Thanks.
  *
- *  Remember...I am not a programmer, everything I do takes a lot of time and research (then MORE research)!
+ *  Remember...I am not a professional programmer, everything I do takes a lot of time and research (then MORE research)!
  *  Donations are never necessary but always appreciated.  Donations to support development efforts are accepted via: 
  *
  *  Paypal at: https://paypal.me/bptworld
@@ -37,41 +37,47 @@
  *
  *  Changes:
  *
- *  V1.0.1 - 05/05/20 - Minor updates
- *  V1.0.0 - 11/01/19 - Initial release
+ *  1.0.2 - 06/24/21 - Adjustments
+ *  1.0.1 - 05/05/20 - Minor updates
+ *  1.0.0 - 11/01/19 - Initial release
  */
 
 metadata {
 	definition (name: "Presence Plus Driver", namespace: "BPTWorld", author: "Bryan Turcotte", importUrl: "https://raw.githubusercontent.com/bptworld/Hubitat/master/Apps/Presence%20Plus/PP-driver.groovy") {
+        capability "Actuator"
+        capability "Contact Sensor"
+        capability "Motion Sensor"
    		capability "Presence Sensor"
         capability "Switch"
 		
-		command "sync", ["string"]
+		//command "initialize"
 
+        attribute "contact", "string"
+        attribute "motion", "string"
 		attribute "presence", "string"
         attribute "since", "string"
         attribute "switch", "string"
 	}
 	preferences() {    	
         section(){
-            input name: "about", type: "paragraph", element: "paragraph", title: "<b>Presence Plus</b>", description: "This device was created by Presence Plus. To be used with the Presence Plus app or any other app that might find this data useful, like Rule Machine!<br>"
+            input name: "about", type: "paragraph", element: "paragraph", title: "<b>Presence Plus</b>", description: "This device was created by Presence Plus. To be used with the Presence Plus app and any other app that might find this data useful, like Rule Machine or Event Engine!<br>"
             input("logEnable", "bool", title: "Enable logging", required: false, defaultValue: false)
         }
     }
 }
 
-def sync(data1) {
-    if(logEnable) log.debug "In Presence Plus Driver - sensorData - Received new data!"
-    if(data1 == "present") on()
-    if(data1 == "notpresent") off()
+def initialize() {
+    state.clear()
 }
 
 def on() {
     if(logEnable) log.info "In Presence Plus Driver - Turning Switch On"
     device.on
     def now = new Date()
-    sendEvent(name: "switch", value: "on", isStateChange: true)
+    sendEvent(name: "contact", value: "open", isStateChange: true)
+    sendEvent(name: "motion", value: "active", isStateChange: true)
     sendEvent(name: "presence", value: "present", isStateChange: true)
+    sendEvent(name: "switch", value: "on", isStateChange: true)    
     sendEvent(name: "since", value: now, displayed: true)
 }
 
@@ -79,7 +85,9 @@ def off() {
     if(logEnable) log.info "In Presence Plus Driver - Turning Switch Off"
     device.off
     def now = new Date()
-    sendEvent(name: "switch", value: "off", isStateChange: true)
+    sendEvent(name: "contact", value: "closed", isStateChange: true)
+    sendEvent(name: "motion", value: "inactive", isStateChange: true)
     sendEvent(name: "presence", value: "not present", isStateChange: true)
+    sendEvent(name: "switch", value: "off", isStateChange: true)
     sendEvent(name: "since", value: now, displayed: true)
 }
