0bc5c26de553192153d5beff7aa1134104d92226
BPTWorld <bryan@bptworld.com>
2024-03-27 21:12:48 -0400

    Update VZM36-Canopy-Driver.groovy

--- Drivers/Inovelli_VZM36_Zigbee_Canopy/VZM36-Canopy-Driver.groovy
+++ Drivers/Inovelli_VZM36_Zigbee_Canopy/VZM36-Canopy-Driver.groovy
@@ -1,4 +1,4 @@
-def getDriverDate() { return "2024-03-21" + " (Basic)" }	// **** DATE OF THE DEVICE DRIVER
+def getDriverDate() { return "2024-03-27" + " (Basic)" }	// **** DATE OF THE DEVICE DRIVER
 /*
 *  Replacement stripped down driver for the Inovelli VZM36 Zigbee Canopy.
 *  
@@ -57,8 +57,6 @@ metadata {
         command "initialize",		   [[name:"create child devices, refresh current states"]]
         command "updateFirmware",	   [[name:"Firmware in this channel may be \"beta\" quality"]]
 
-		fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0004,0005,0006,0008,0702,0B04,0B05,FC57,FC31", outClusters:"0003,0019",           model:"VZM31-SN", manufacturer:"Inovelli"
-		fingerprint profileId:"0104", endpointId:"02", inClusters:"0000,0003",                                              outClusters:"0003,0019,0006,0008", model:"VZM31-SN", manufacturer:"Inovelli"
 		fingerprint profileId:"0104", endpointId:"01", inClusters:"0000,0003,0004,0005,0006,0008,0B05,1000,FC31,FC57", outClusters:"0019", model:"VZM36", manufacturer:"Inovelli"
     }
 
